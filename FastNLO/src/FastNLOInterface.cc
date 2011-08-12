c0d2e15d70753e9530627f91997bcbfa6d6f192c
britzger <britzger@b2a8d302-ca0a-427a-b7b9-081adbce06bc>
2011-08-12 19:56:10 +0000

    Update+Cleanup. Passing the alpha_s(mz) starting scale from QCDNUM to FastNLO.
    git-svn-id: https://svnsrv.desy.de/desy/h1fitter/trunk@276 b2a8d302-ca0a-427a-b7b9-081adbce06bc

--- FastNLO/src/FastNLOInterface.cc
+++ FastNLO/src/FastNLOInterface.cc
@@ -19,63 +19,64 @@
 #include <string>
 #include <map>
 #include <FastNLOReader.h>
+#include <cmath>
 
 using namespace std;
 
 
 extern "C" {
-  int fastnloinit_(const char *s, const int *idataset, const char *thfile );
-  int fastnlocalc_(const int *idataset, double *xsec);
+   int fastnloinit_(const char *s, const int *idataset, const char *thfile );
+   int fastnlocalc_(const int *idataset, double *xsec);
+   int getalf_( double* alfs, double* r2 );
 }
 
 map<int, FastNLOReader*> gFastNLO_array;
 
 int fastnloinit_(const char *s, const int *idataset, const char *thfile  ) {
 
-  map<int, FastNLOReader*>::const_iterator FastNLOIterator = gFastNLO_array.find(*idataset);
-  if(FastNLOIterator != gFastNLO_array.end( )) 
-    return 1;
+   map<int, FastNLOReader*>::const_iterator FastNLOIterator = gFastNLO_array.find(*idataset);
+   if(FastNLOIterator != gFastNLO_array.end( )) 
+      return 1;
   
-  FastNLOReader* fnloreader = NULL;
-  if(string(s).compare( "H1 inclusive jet 99-00 data"))
-    fnloreader = new FastNLOReader( thfile );  
-  else {
-    cerr << "fastnloinit did not recognize data set <" << s << ">" <<  endl;
-    return 1;
-  }
-    
-  //fnloreader->Print();
-  fnloreader->SetPDFInterface(FastNLOReader::kH1FITTER);
-  fnloreader->SetAlphasEvolution(FastNLOReader::kNLOJET);
-  fnloreader->SetScaleVariation(0);
-  fnloreader->SetAlphasMz( 0.1178 );
+   FastNLOReader* fnloreader = NULL;
+   if(string(s).compare( "H1 inclusive jet 99-00 data"))
+      fnloreader = new FastNLOReader( thfile );  
+   else {
+      cerr << "fastnloinit did not recognize data set <" << s << ">" <<  endl;
+      return 1;
+   }
+   
+   fnloreader->SetPDFInterface(FastNLOReader::kH1FITTER);
+   //fnloreader->SetAlphasEvolution(FastNLOReader::kQCDNUMInternal); // fully consistend alpha_s evolution has to be implemented.
+   fnloreader->SetScaleVariation(0);
 
-  //fnloreader->FillPDFCache();   // pdf is 'external'! you always have to call FillPDFCache();
-  
-  gFastNLO_array.insert(pair<int, FastNLOReader*>(*idataset, fnloreader) );
-  return 0;
+   gFastNLO_array.insert(pair<int, FastNLOReader*>(*idataset, fnloreader) );
+   return 0;
 }
 
 
 int fastnlocalc_(const int *idataset, double *xsec) {
 
 
-  map<int, FastNLOReader*>::const_iterator FastNLOIterator = gFastNLO_array.find(*idataset);
-  if(FastNLOIterator == gFastNLO_array.end( )) 
-    return 1;
+   map<int, FastNLOReader*>::const_iterator FastNLOIterator = gFastNLO_array.find(*idataset);
+   if(FastNLOIterator == gFastNLO_array.end( )) 
+      return 1;
 
-  FastNLOReader* fnloreader = FastNLOIterator->second;
-  fnloreader->FillPDFCache();   // pdf is 'external'! you always have to call FillPDFCache();
+   FastNLOReader* fnloreader = FastNLOIterator->second;
 
-  fnloreader->CalcCrossSection();
+   double alfsMz= 0;
+   double Mz2= 0.;
+   getalf_(&alfsMz,&Mz2);
+   fnloreader->SetAlphasMz( alfsMz );
+   fnloreader->FillPDFCache();			// pdf is 'external'! you always have to call FillPDFCache();
+
+   fnloreader->CalcCrossSection();
+
+   vector < double > xs = fnloreader->GetXSection();
+ 
+   for ( unsigned i=0;i<xs.size();i++){
+      xsec[i] = xs[i];
+   }
   
-  vector < double > xs = fnloreader->GetXSection();
-  vector < double > xsref = fnloreader->GetReferenceXSection();
-
-  for ( unsigned i=0;i<xs.size();i++){
-    xsec[i] = xs[i];
-    //cout << "i="<<i<<"\txs="<<xs[i]<<"\tref="<<xsref[i]<<endl;
-  }
-  
-  return 0;
+   return 0;
 }
