8810a05aaad159d5d660edb3ed0a4cbdf5a5c689
voica <voica@b2a8d302-ca0a-427a-b7b9-081adbce06bc>
2011-08-12 15:23:30 +0000

    minuit cleaned in
    git-svn-id: https://svnsrv.desy.de/desy/h1fitter/trunk@256 b2a8d302-ca0a-427a-b7b9-081adbce06bc

--- src/minuit_ini.f
+++ src/minuit_ini.f
@@ -3,13 +3,7 @@
       character*72 minfile
 
       open ( 25, file='output/minuit.out.txt' )
-      if(itheory.eq.0) then 
-         minfile='minuit.in.collfact.txt' 
-         minfile='minuit.in.txt' 
-      elseif(itheory.eq.1) then
-         minfile='minuit.in.ktfact.txt'
-      else
-         minfile='minuit.in.txt' 
+      minfile='minuit.in.txt' 
       endif
       write(6,*) ' read minuit input params from file ',minfile
       open ( 24, file=minfile )
