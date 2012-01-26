685369c2d78cb21192dec62d500417bd9c7cd680
britzger <britzger@b2a8d302-ca0a-427a-b7b9-081adbce06bc>
2012-01-26 09:26:25 +0000

    update
    git-svn-id: https://svnsrv.desy.de/desy/h1fitter/trunk@452 b2a8d302-ca0a-427a-b7b9-081adbce06bc

--- FastNLO/src/FastNLOInterface.cc
+++ FastNLO/src/FastNLOInterface.cc
@@ -34,21 +34,62 @@ map<int, FastNLOReader*> gFastNLO_array;
 
 int fastnloinit_(const char *s, const int *idataset, const char *thfile  ) {
 
+   //cout << "FastNLOINterface::fastnloinit_ idataset = "<<*idataset<<endl;
+
    map<int, FastNLOReader*>::const_iterator FastNLOIterator = gFastNLO_array.find(*idataset);
    if(FastNLOIterator != gFastNLO_array.end( )) 
       return 1;
   
    FastNLOReader* fnloreader = NULL;
-   if(string(s).compare( "H1 inclusive jet 99-00 data"))
-      fnloreader = new FastNLOReader( thfile );  
-   else {
-      cerr << "fastnloinit did not recognize data set <" << s << ">" <<  endl;
-      return 1;
-   }
+   fnloreader = new FastNLOReader( thfile );  
+   //    if(string(s).compare( "H1 inclusive jet 99-00 data"))
+   //       fnloreader = new FastNLOReader( thfile );  
+   //    else {
+   //       cerr << "fastnloinit did not recognize data set <" << s << ">" <<  endl;
+   //       return 1;
+   //    }
    
+   
+   fnloreader->SetUnits(FastNLOReader::kAbsoluteUnits);
+   if ( string(s).find("D0 pp jets") == 0 ){;
+      cout << "D0 -> Setting to publication units."<<endl;
+      fnloreader->SetUnits(FastNLOReader::kPublicationUnits);
+   }
+   else if ( string(s).find("CMS inclusive jets") == 0 ){;
+      cout << "D0 -> Setting to publication units."<<endl;
+      fnloreader->SetUnits(FastNLOReader::kPublicationUnits);
+   }
+   else{
+      cout << "could not recognize D0 jets. Using absolute units."<<endl;
+   }
+
    fnloreader->SetPDFInterface(FastNLOReader::kH1FITTER);
-   //fnloreader->SetAlphasEvolution(FastNLOReader::kQCDNUMInternal); // fully consistend alpha_s evolution has to be implemented.
-   //fnloreader->SetScaleVariation(0);
+   fnloreader->SetAlphasMz( 0.1180 ); // just for initalisation
+   fnloreader->SetAlphasEvolution(FastNLOReader::kQCDNUMInternal); // fully consistend alpha_s evolution has to be implemented.
+
+   // looking for scale factor = 1!
+   int nscale = fnloreader->GetNScaleVariations();
+   vector<double> scalefactors = fnloreader->GetScaleFactors();
+   for (int iscale = 0;iscale < nscale ;iscale++) {
+      if ( fabs(scalefactors[iscale] - 1.) < 0.0001 ){
+	 fnloreader->SetScaleVariation(iscale);
+      }
+   }
+
+   // switching non-pert corr off
+   fnloreader->SetContributionON(FastNLOReader::kNonPerturbativeCorrection,0,false);
+   fnloreader->SetContributionON(FastNLOReader::kNonPerturbativeCorrection,1,false);
+
+   // no threshold corrections
+   //fnloreader->SetContributionON(FastNLOReader::kThresholdCorrection,0,false);
+
+
+   fnloreader->FillAlphasCache();
+   fnloreader->FillPDFCache();			// pdf is 'external'! you always have to call FillPDFCache();
+   fnloreader->CalcCrossSection();
+   //fnloreader->PrintCrossSectionsLikeFreader();
+
+
 
    gFastNLO_array.insert(pair<int, FastNLOReader*>(*idataset, fnloreader) );
    return 0;
@@ -57,7 +98,10 @@ int fastnloinit_(const char *s, const int *idataset, const char *thfile  ) {
 
 int fastnlocalc_(const int *idataset, double *xsec) {
 
+   cout << "FastNLOINterface::fastnlocalc_ idataset = " <<*idataset<<endl;
 
+   // call QCDNUM::_evaluate here!
+   
    map<int, FastNLOReader*>::const_iterator FastNLOIterator = gFastNLO_array.find(*idataset);
    if(FastNLOIterator == gFastNLO_array.end( )) 
       return 1;
@@ -68,11 +112,13 @@ int fastnlocalc_(const int *idataset, double *xsec) {
    double Mz2= 0.;
    getalf_(&alfsMz,&Mz2);
    fnloreader->SetAlphasMz( alfsMz );
+   fnloreader->FillAlphasCache();
    fnloreader->FillPDFCache();			// pdf is 'external'! you always have to call FillPDFCache();
 
    fnloreader->CalcCrossSection();
+   //fnloreader->PrintCrossSectionsLikeFreader();
 
-   vector < double > xs = fnloreader->GetXSection();
+   vector < double > xs = fnloreader->GetCrossSection();
  
    for ( unsigned i=0;i<xs.size();i++){
       xsec[i] = xs[i];
