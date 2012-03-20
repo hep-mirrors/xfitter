0179dfcf3db18fd70d93b9d0da5292be67a60e0b
jtomasz <jtomasz@b2a8d302-ca0a-427a-b7b9-081adbce06bc>
2012-03-20 14:31:06 +0000

    extra files
    git-svn-id: https://svnsrv.desy.de/desy/h1fitter/trunk@499 b2a8d302-ca0a-427a-b7b9-081adbce06bc

---
+++ src/FixModelParams.f
@@ -0,0 +1,47 @@
+c=====================================================
+      subroutine DDIS_FixModelParams(p)
+C-
+C (to be called fron fcn. parsminuit is a vector of minuit parameters.
+C
+      Implicit none 
+      include 'extrapars.inc'
+
+      integer GetParameterIndex
+      double precision p(*)
+      double precision pars(3)
+      integer idxPomeron_a0,idxReggeon_factor,idxReggeon_a0
+
+      idxPomeron_a0 = GetParameterIndex('Pomeron_a0')
+      if ( idxPomeron_a0.eq.0) then
+         print *,'Did not find Pomeron_a0 parameter'
+         print *,'Add to ExtraParamters with the name  Pomeron_a0'
+         call HF_stop
+      else
+         idxPomeron_a0 = iExtraParamMinuit(idxPomeron_a0)
+      endif
+      pars(1)=p(idxPomeron_a0) 
+      
+      idxReggeon_factor = GetParameterIndex('Reggeon_factor')
+      if ( idxReggeon_factor.eq.0) then
+         print *,'Did not find Reggeon_factor parameter'
+         print *,'Add to ExtraParamters with the name Reggeon_factor'
+            call HF_stop
+      else
+         idxReggeon_factor = iExtraParamMinuit(idxReggeon_factor)
+      endif
+      pars(2)=p(idxReggeon_factor) 
+
+
+      idxReggeon_a0 = GetParameterIndex('Reggeon_a0')
+      if ( idxReggeon_a0.eq.0) then
+         print *,'Did not find Reggeon_a0 parameter'
+         print *,'Add to ExtraParamters with the name Reggeon_a0'
+            call HF_stop
+      else
+         idxReggeon_a0 = iExtraParamMinuit(idxReggeon_a0)
+      endif
+      pars(3)=p(idxReggeon_a0) 
+
+      call DDISsetParams(pars)
+      
+      end
