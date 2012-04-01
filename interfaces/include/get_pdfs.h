4a5419d2088105681b4bde649dd48cab2141687d
glazov <glazov@b2a8d302-ca0a-427a-b7b9-081adbce06bc>
2012-04-01 16:37:47 +0000

    fortran c and c++ intefaces for pdf calls
    git-svn-id: https://svnsrv.desy.de/desy/h1fitter/trunk@573 b2a8d302-ca0a-427a-b7b9-081adbce06bc

---
+++ interfaces/include/get_pdfs.h
@@ -0,0 +1,25 @@
+#ifndef get_pdfs_h
+#define get_pdfs_h
+
+// Define interfaces to PDF calls
+
+//
+// Function to obtain PDFs
+// 
+#define HF_GET_PDFS_WRAP F77_FUNC_ (hf_get_pdfs, HF_GET_PDFS)
+#ifdef __cplusplus
+extern "C"  /* prevent C++ name mangling */
+#endif
+void HF_GET_PDFS_WRAP(const double *x, const double *q2, double *pdfs);
+
+
+//
+// Function to get alphaS
+//
+#define HF_GET_ALPHAS_WRAP F77_FUNC_ (hf_get_alphas, HF_GET_ALPHAS)
+#ifdef __cplusplus
+extern "C"  /* prevent C++ name mangling */
+#endif
+double HF_GET_ALPHAS_WRAP(const double *q2);
+
+#endif
