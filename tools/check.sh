49c9dc17993df991962163c98d7a4c1d37d2b2c4
voica <voica@b2a8d302-ca0a-427a-b7b9-081adbce06bc>
2013-02-18 14:45:32 +0000

    make sh executable
    git-svn-id: https://svnsrv.desy.de/desy/h1fitter/trunk@1001 b2a8d302-ca0a-427a-b7b9-081adbce06bc

--- tools/check.sh
+++ tools/check.sh
@@ -1,6 +1,44 @@
 #!/bin/sh
 #
 #   Turn on Verbose Mode
-set -x
+#set -x
 
-cp ../input_
+rm -rf temp
+mkdir temp
+
+echo "========================================"
+echo "Running checks"
+echo "========================================"
+echo "validation test: "
+echo "PASS if code runs properly DIS code and produces HERAPDF1.0"
+echo "FAIL if code fails to reproduce HERAPDF1.0"
+echo "========================================"
+
+
+cp input_steering/minuit.in.txt.10pHERAPDF minuit.in.txt
+cp input_steering/steering.txt.def steering.txt
+
+bin/FitPDF >/dev/null
+
+grep  'After' output/Results.txt > temp/out.txt
+grep  'After' examples/output/Results.txt > temp/def.txt
+
+diff temp/out.txt temp/def.txt 
+
+exitcode=$?
+
+if [ $exitcode = 0 ]; then
+echo "========================================"
+echo "Check is fine"
+echo "========================================"
+else
+echo "========================================"
+echo -e "Failed validation with default steering"
+echo "========================================"
+exit 1
+fi 
+echo "========================================"
+
+rm -rf temp
+
+
