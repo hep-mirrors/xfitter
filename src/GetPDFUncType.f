93ed019a282345e7b964354ae2f38f1e3765b68d
glazov <glazov@b2a8d302-ca0a-427a-b7b9-081adbce06bc>
2014-01-18 08:40:20 +0000

    By-name recognition of -MC, _MC, MC-, MC_ and EIGSYM sets
    git-svn-id: https://svnsrv.desy.de/desy/h1fitter/trunk@1309 b2a8d302-ca0a-427a-b7b9-081adbce06bc

--- src/GetPDFUncType.f
+++ src/GetPDFUncType.f
@@ -6,12 +6,19 @@ c Adapted from LHAPDF uncertainties.f
       if ((name(:5).eq.'NNPDF')
      $     .or.(name(:7).eq.'Alekhin')
      $     .or.(name(:5).eq.'Botje')
-     $     .or.(name(:5).eq.'Fermi')) then ! Monte Carlo PDF sets
+     $     .or.(name(:5).eq.'Fermi')
+     $     .or.(index(name,'_MC').gt.0)
+     $     .or.(index(name,'MC_').gt.0)
+     $     .or.(index(name,'-MC').gt.0)
+     $     .or.(index(name,'MC-').gt.0)
+     $     ) then               ! Monte Carlo PDF sets
          lMonteCarlo = .true.
          lSymmetric = .true.
       else if ((name(:4).eq.'A02M').or.(name(:4).eq.'a02m')
      $        .or.(name(:6).eq.'ABKM09').or.(name(:6).eq.'abkm09')
-     $        .or.(name(:5).eq.'ABM11').or.(name(:5).eq.'abm11')) then ! symmetric eigenvector PDF sets
+     $        .or.(name(:5).eq.'ABM11').or.(name(:5).eq.'abm11')
+     $     .or.(index(name,'EIGSYM').gt.0)
+     $        ) then            ! symmetric eigenvector PDF sets
          lMonteCarlo = .false.
          lSymmetric = .true.
       else                      ! default: assume asymmetric Hessian eigenvector PDF sets
