5e698a9c6aeae0165c5d25555f8d198ddf431ec0
camarda <camarda@b2a8d302-ca0a-427a-b7b9-081adbce06bc>
2015-12-03 20:08:58 +0000

    - added PACKAGE_URL variable - use libtool to build libraries - added --disable-root option (root is enabled by default) - fixed banner style
    git-svn-id: https://svnsrv.desy.de/desy/h1fitter/trunk@1937 b2a8d302-ca0a-427a-b7b9-081adbce06bc

--- src/hfbanner.cc
+++ src/hfbanner.cc
@@ -4,26 +4,26 @@
 using namespace std;
 void hfbanner_()
 {
- cout << "                   FFFFFFFFFFFFFFFFFFFFF  iiii         tttt               tttt                                                   " << endl;
- cout << "                   F:::::::::::::::::::F  iiii        t::::t             t::::t                                                              " << endl;
- cout << "                    F::::::FFFFFFFFF:::F              t::::t             t::::t                                                     " << endl;
- cout << " xxxxxx     xxxxxx  F::::F          FFFF iiiiii ttttttt::::ttttttt ttttttt::::ttttttt        eeeeeeeeeee     rrrr   rrrrrrr  " << endl; 
- cout << " x::::x    x::::x   F::::F               i::::i t::::::::::::::::t t::::::::::::::::t      e::::::::::::e   r:::rrr:::::::r  " << endl;
- cout << "  x::::x  x::::x    F:::::FFFFFFFFFF     i::::i  ttttt:::::tttttt   ttttt:::::tttttt     e:::::eeeee:::::e  r:::::::::::::r  " << endl;
- cout << "   x::::xx::::x     F::::::::::::::F     i::::i       t::::t             t::::t          e::::e     e::::e r::::::rrrr::::r  " << endl;
- cout << "    x::::::::x      F:::::FFFFFFFFFF     i::::i       t::::t             t::::t          e::::::eeeee::::e r::::r     r:::r  " << endl;
- cout << "     x::::::x       F::::F               i::::i       t::::t             t::::t          e::::::::::::::e  r::::r     rrrrr  " << endl;
- cout << "     x::::::x       F::::F               i::::i       t::::t             t::::t          e::::eeeeeeeeee   r::::r            " << endl;
- cout << "    x::::::::x      F::::F               i::::i       t::::t    ttttt    t::::t    ttttt e::::e            r::::r            " << endl;
- cout << "   x::::xx::::x    F::::::F             i::::::i      t::::t    t:::t    t::::t    t:::t e::::e            r::::r            " << endl;
- cout << "  x::::x  x::::x   F::::::F             i::::::i      t:::::tttt::::t    t:::::tttt::::t e::::::eeeeeeee   r::::r            " << endl;
- cout << " x::::x    x::::x  F::::::F             i::::::i       t:::::::::::t      t:::::::::::t    e::::::::::::e  r::::r            " << endl;
- cout << "xxxxxx      xxxxxx FFFFFFFF             iiiiiiii        ttttttttttt         tttttttttt      eeeeeeeeeeee   rrrrrr            " << endl;
+ cout << "                     FFFFFFFFFFFFFFFFFFFFFF  iiii          tttt               tttt                                                  " << endl;
+ cout << "                     F::::::::::::::::::::F i::::i      ttt:::t            ttt:::t                                                  " << endl;
+ cout << "                     F::::::::::::::::::::F  iiii       t:::::t            t:::::t                                                  " << endl;
+ cout << "                     FF::::::FFFFFFFFF::::F             t:::::t            t:::::t                                                  " << endl;
+ cout << " xxxxxxx      xxxxxxx  F:::::F       FFFFFFiiiiiiittttttt:::::tttttttttttttt:::::ttttttt        eeeeeeeeeeee    rrrrr   rrrrrrrrr   " << endl;
+ cout << "  x:::::x    x:::::x   F:::::F             i:::::it:::::::::::::::::tt:::::::::::::::::t      ee::::::::::::ee  r::::rrr:::::::::r  " << endl;
+ cout << "   x:::::x  x:::::x    F::::::FFFFFFFFFF    i::::it:::::::::::::::::tt:::::::::::::::::t     e::::::eeeee:::::eer:::::::::::::::::r " << endl;
+ cout << "    x:::::xx:::::x     F:::::::::::::::F    i::::itttttt:::::::tttttttttttt:::::::tttttt    e::::::e     e:::::err::::::rrrrr::::::r" << endl;
+ cout << "     x::::::::::x      F:::::::::::::::F    i::::i      t:::::t            t:::::t          e:::::::eeeee::::::e r:::::r     r:::::r" << endl;
+ cout << "      x::::::::x       F::::::FFFFFFFFFF    i::::i      t:::::t            t:::::t          e:::::::::::::::::e  r:::::r     rrrrrrr" << endl;
+ cout << "      x::::::::x       F:::::F              i::::i      t:::::t            t:::::t          e::::::eeeeeeeeeee   r:::::r            " << endl;
+ cout << "     x::::::::::x      F:::::F              i::::i      t:::::t    tttttt  t:::::t    tttttte:::::::e            r:::::r            " << endl;
+ cout << "    x:::::xx:::::x   FF:::::::FF           i::::::i     t::::::tttt:::::t  t::::::tttt:::::te::::::::e           r:::::r            " << endl;
+ cout << "   x:::::x  x:::::x  F::::::::FF           i::::::i     tt::::::::::::::t  tt::::::::::::::t e::::::::eeeeeeee   r:::::r            " << endl;
+ cout << "  x:::::x    x:::::x F::::::::FF           i::::::i       tt:::::::::::tt    tt:::::::::::tt  ee:::::::::::::e   r:::::r            " << endl;
+ cout << " xxxxxxx      xxxxxxxFFFFFFFFFFF           iiiiiiii         ttttttttttt        ttttttttttt      eeeeeeeeeeeeee   rrrrrrr            " << endl;
  cout << endl;
  cout << "  Version " << VERSION << endl;
- cout << "  http://xfitter.org                             " <<  PACKAGE_BUGREPORT << endl;
+ cout << "  " << PACKAGE_URL << "                             " <<  PACKAGE_BUGREPORT << endl;
  cout << "----------------------------------------------------------------------------------------" << endl;
  cout << endl;
  cout << endl;
 }
-
