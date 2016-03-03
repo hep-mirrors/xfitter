8019ca89039dd18f38af632991c78d720909fda1
britzger <britzger@b2a8d302-ca0a-427a-b7b9-081adbce06bc>
2016-03-03 11:26:51 +0000

    adjust warning/error message for photon PDF.
    git-svn-id: https://svnsrv.desy.de/desy/h1fitter/trunk@2074 b2a8d302-ca0a-427a-b7b9-081adbce06bc

--- FastNLO/src/fastNLOReader.cc
+++ FastNLO/src/fastNLOReader.cc
@@ -1578,7 +1578,7 @@ bool fastNLOReader::TestAlphas() {
 //______________________________________________________________________________
 bool fastNLOReader::TestXFX() {
    vector<double> pdftest = GetXFX(1.e-2,10);
-   if (pdftest.size() != 13) {
+   if (pdftest.size() <= 13) {
       error["TestXFX"]<<"The pdf array must have the size of 13 flavors.\n";
       return false;
    }
