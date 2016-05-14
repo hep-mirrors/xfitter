4aab4a1c5651e2a35478c2fed46a9ff3c1113c2b
SG <glazov@mail.desy.de>
2016-05-14 09:44:37 +0200

    some cosmetics

--- python/testPlot.py
+++ python/testPlot.py
@@ -9,6 +9,8 @@ from matplotlib.pyplot import *
 rcParams['ytick.labelsize'] = 14
 rcParams['xtick.labelsize'] = 14
 rc('font',family='serif')
+#import seaborn
+#seaborn.set_style(style="white")
 
 # load default PDF
 aa = plot_lhapdf.loadPDF()
@@ -17,8 +19,8 @@ figure()
 x = 0.3
 qq = arange(1.,100.,0.1)
 plot(qq,plot_lhapdf.fq(aa,1,x,qq)-plot_lhapdf.fq(aa,-1,x,qq))
-xlabel('q, GeV',size=15)
-ylabel('$u_v$',size=15)
+xlabel('q, GeV',size=20)
+ylabel('$u_v$',size=20)
 
 savefig("uv.pdf")
 
@@ -28,7 +30,8 @@ q = 1.9
 plot(xx, plot_lhapdf.fx(aa,1,10**xx,q),label="$u$",linewidth=2)
 plot(xx, plot_lhapdf.fx(aa,2,10**xx,q),label="$d$",linewidth=2)
 plot(xx, plot_lhapdf.fx(aa,3,10**xx,q),label="$s$",linewidth=2)
-legend()
-xlabel('$ \log_{10} x$', size=15)
+axis([-4.,0.,0.,1.05])
+legend(fontsize=18)
+xlabel('$ \log_{10} x$', size=20)
 savefig("pdfs.pdf")
 
