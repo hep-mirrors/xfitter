f6e3c109d3a895031382c7c6a59573ab4a3d5591
Daniel Britzger <britzger@nafhh-atlas01.desy.de>
2016-12-06 10:07:06 +0100

    reaction: fastNLO, wich already creates an fastNLO object

---
+++ reactions/fastNLO/include/ReactionfastNLO.h
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#include "ReactionTheory.h"
+
+/**
+  @class' ReactionfastNLO
+
+  @brief A wrapper class for fastNLO reaction 
+
+  Based on the ReactionTheory class. Reads options produces 3d cross section.
+
+  @version 0.1
+  @date 2016-12-06
+  */
+
+class ReactionfastNLO : public ReactionTheory
+{
+  public:
+    ReactionfastNLO(){};
+
+//    ~ReactionfastNLO(){};
+//    ~ReactionfastNLO(const ReactionfastNLO &){};
+//    ReactionfastNLO & operator =(const ReactionAfastNLO &r){return *(new ReactionfastNLO(r));};
+
+  public:
+    virtual string getReactionName() const { return  "fastNLO" ;};
+    void initAtStart(const string &); 
+    virtual int compute(valarray<double> &val, map<string, valarray<double> > &err);
+  protected:
+    virtual int parseOptions(){ return 0;};
+};
+
---
+++ reactions/fastNLO/src/ReactionfastNLO.cc
@@ -0,0 +1,33 @@
+ 
+/*
+   @file ReactionfastNLO.cc
+   @date 2016-12-06
+   @author  AddReaction.py
+   Created by  AddReaction.py on 2016-12-06
+*/
+
+#include "ReactionfastNLO.h"
+//#include "FastNLOxfReactionTheory.h" // todo
+#include "fastnlotk/fastNLOReader.h"
+
+
+// the class factories
+extern "C" ReactionfastNLO* create() {
+  return new ReactionfastNLO();
+}
+
+
+// Initialize at the start of the computation
+void ReactionfastNLO::initAtStart(const string &s)
+{
+   // for testing purposes: create an object here
+   fastNLOTable* fnlo = new fastNLOTable();
+   fnlo->PrintHeader();
+}
+
+// Main function to compute results at an iteration
+int ReactionfastNLO::compute(valarray<double> &val, map<string, valarray<double> > &err)
+{
+   return 0;
+}
+
