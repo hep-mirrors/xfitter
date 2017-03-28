301aeb7270f807ac39d4156a884aa694b35a6d17
Alexandre Glazov <alexandre.glazov@desy.de>
2017-03-28 23:13:13 +0200

    first working APPLgrid reaction interface. Make -std=c++11 to be default option

---
+++ reactions/APPLgrid/include/ReactionAPPLgrid.h
@@ -0,0 +1,40 @@
+
+#pragma once
+
+#include "ReactionTheory.h"
+#include "appl_grid/appl_grid.h"
+#include <memory>
+
+/**
+  @class' ReactionAPPLgrid
+
+  @brief A wrapper class for APPLgrid reaction 
+
+  Based on the ReactionTheory class. Reads options produces 3d cross section.
+
+  @version 0.1
+  @date 2017-03-28
+  */
+
+class ReactionAPPLgrid : public ReactionTheory
+{
+  public:
+    ReactionAPPLgrid(){};
+
+//    ~ReactionAPPLgrid(){};
+//    ~ReactionAPPLgrid(const ReactionAPPLgrid &){};
+//    ReactionAPPLgrid & operator =(const ReactionAAPPLgrid &r){return *(new ReactionAPPLgrid(r));};
+
+ public:
+    virtual string getReactionName() const { return  "APPLgrid" ;};
+    int initAtStart(const string &); 
+    virtual void setDatasetParamters( int dataSetID, map<string,string> pars) ;
+    virtual int compute(int dataSetID, valarray<double> &val, map<string, valarray<double> > &err);
+ protected:
+    virtual int parseOptions(){ return 0;};    
+ private:
+    map<int, std::shared_ptr<appl::grid> > _grids;
+    map<int, int> _order;
+    map<int, double> _muR, _muF; // !> renormalisation and factorisation scales
+};
+
---
+++ reactions/APPLgrid/src/ReactionAPPLgrid.cc
@@ -0,0 +1,94 @@
+/*
+  @file ReactionAPPLgrid.cc
+  @date 2017-03-28
+  @author  AddReaction.py
+  Created by  AddReaction.py on 2017-03-28
+*/
+
+#include "ReactionAPPLgrid.h"
+
+// some hacking, to transfer XFX and alpha_S from class
+pOneParFunc gAs  = nullptr;
+pXFXlike    gXFX = nullptr;
+
+ // the class factories
+extern "C" ReactionAPPLgrid* create() {
+  return new ReactionAPPLgrid();
+}
+
+
+// Place here temporary. Should go in simplified xfitter_cpp.h-like.
+extern "C" {
+  void hf_errlog_(const int &id, const char text[], int);
+}
+int OrderMap(std::string ord) {
+  std::map <string,int> mOrderMap  = { {"NNNLO",4}, {"NNLO",3}, {"NLO",2}, {"LO",1} };
+  if ( mOrderMap.find(ord) != mOrderMap.end() ) {
+    return mOrderMap[ord];
+  }
+  else {
+    string text = "F: Can not convert "+ord+" to computation order";
+    hf_errlog_(17032803,text.c_str(),text.size());
+    return -1;
+  }
+}
+
+
+ // Initialize at the start of the computation
+int ReactionAPPLgrid::initAtStart(const string &s )
+{
+   gXFX = getXFX();
+   gAs  = alpha_S;
+   return 0;
+}
+
+ // Initialisze for a given dataset:
+void ReactionAPPLgrid::setDatasetParamters(int dataSetID, map<string,string> pars) {
+// Get grid name:
+   if ( pars.find("GridName") != pars.end() )  {
+     try {
+        std::shared_ptr<appl::grid>  g(new appl::grid(pars["GridName"])); 
+        g->trim();
+        _grids[dataSetID] = g;
+     }
+     catch ( const std::exception& e ) {
+        std::string text = "F:Failed to read APPLgrid file"+pars["GridName"];
+        hf_errlog_(17032802,text.c_str(),text.size());
+     }
+   }
+   else {
+     std::string text = "F:GridName must be specified for the Reaction APPLgrid";
+     hf_errlog_(17032801,text.c_str(),text.size());
+   }
+
+// Determine order
+   int order = OrderMap( GetParamS("Order"));  // Global order
+   if (pars.find("Order") != pars.end() ) { // Local order
+       int localOrder = OrderMap( pars["Order"] );
+       order = localOrder>order ? order : localOrder;
+   }
+   _order[dataSetID] = order;
+// Determine MuR and MuF.  Use default 
+  _muR[dataSetID] =  pars.find("muR") == pars.end() ? GetParam("muR") : stod(pars["muR"]);
+  _muF[dataSetID] =  pars.find("muF") == pars.end() ? GetParam("muF") : stod(pars["muF"]);
+
+  if (_muR[dataSetID] == 0) _muR[dataSetID] = 1.0; 
+  if (_muF[dataSetID] == 0) _muF[dataSetID] = 1.0;
+
+}
+
+
+ // Main function to compute results at an iteration
+int ReactionAPPLgrid::compute(int dataSetID, valarray<double> &val, map<string, valarray<double> > &err) {
+ // Convolute the grid:
+   std::vector<double> vals =  _grids[dataSetID]->vconvolute( gXFX, gAs, _order[dataSetID]-1, _muR[dataSetID], _muF[dataSetID] );
+
+   int j=0; 
+   for (std::size_t i=0; i<vals.size(); i++) {
+      if ( notMasked(dataSetID, i))  {
+	 val[j++] = vals[i];
+      }
+   }
+  return 0;
+}
+
