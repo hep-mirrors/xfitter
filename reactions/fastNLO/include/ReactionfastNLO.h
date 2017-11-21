c8157c977effb97f3915e32c73f980a6d9fe0a68
Daniel Britzger <britzger@nafhh-atlas02.desy.de>
2017-11-21 15:00:28 +0100

    update fastNLO to deal with multiple datasets, as well as fastNLO files for a single data set.

--- reactions/fastNLO/include/ReactionfastNLO.h
+++ reactions/fastNLO/include/ReactionfastNLO.h
@@ -1,22 +1,38 @@
+#ifndef xFitter_ReactionfastNLO
+#define xFitter_ReactionfastNLO
 
 #pragma once
 
 #include "ReactionTheory.h"
+//#include "fastNLOReaction.h"
 //#include <memory> 
-#include "fastnlotk/fastNLOReader.h" // this is still the old interface
+#include "fastnlotk/fastNLOReader.h" 
+#include <map>
+#include <vector>
 
 /**
   @class' ReactionfastNLO
 
   @brief A wrapper class for fastNLO reaction 
 
-  Based on the ReactionTheory class. Reads options produces 3d cross section.
+  Based on the ReactionTheory class
 
-  @version 0.1
+  @version 0.3
   @date 2016-12-06
   */
 
-class ReactionfastNLO : public ReactionTheory, public fastNLOReader {
+class fastNLOReaction : public fastNLOReader {
+public:
+   fastNLOReaction(string name, ReactionTheory* reaction) : fastNLOReader(name) {fReaction=reaction;};
+protected:
+   fastNLOReaction(string name) : fastNLOReader(name) {}; // not public!
+   ReactionTheory* fReaction=NULL;
+   double EvolveAlphas(double Q ) const { return fReaction->alphaS(Q); } //!< provide alpha_s to fastNLO      
+   bool InitPDF() { return true;}; //!< required by fastNLO
+   vector<double> GetXFX(double xp, double muf) const { return fReaction->xfx(xp,muf); }//!< provide PDFs to fastNLO
+};
+
+class ReactionfastNLO : public ReactionTheory {
 public:
     ReactionfastNLO(){};
 
@@ -33,9 +49,7 @@ protected:
    virtual int parseOptions(){ return 0;};
    //std::unique_ptr<FastNLOxFitter> fnlo;
    // fastNLO inherited functions 
-   double EvolveAlphas(double Q ) const { return this->alphaS(Q); } //!< provide alpha_s to fastNLO      
-   bool InitPDF() { return true;}; //!< required by fastNLO
-   vector<double> GetXFX(double xp, double muf) const { return this->xfx(xp,muf); }//!< provide PDFs to fastNLO
-
+   std::map<int,std::vector<fastNLOReaction*> > ffnlo;
 };
 
+#endif
