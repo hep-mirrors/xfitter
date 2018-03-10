bd9287c450d1082b73cfbb478d4554e41954d225
Sasha Glazov <glazov@mail.desy.de>
2018-03-10 12:19:58 +0100

    modified slightly example

--- testPythonInterface.py
+++ testPythonInterface.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python
-
 import sys
-
 sys.setdlopenflags(256|1)   # MAGIC !!!
 
-# at the moment xfitter.so file is  located in ./src directory:
+# at the moment xfitter.so file is  located in ./lib directory:
 sys.path.append("./lib")
 
 import libxfitter_fit as xfitter
 
 chi2l = []
+theo1 = []
 
 class a(xfitter.Action):
-    def Initialize(self):
-        print "haha"
     def AtIteration(self):
-        chi2 = xfitter.chi2()
-        print "hoho ",chi2
-        chi2l.append(chi2)
-
+        chi2l.append(xfitter.chi2())
+        theo1.append(xfitter.theo(1))
+        
 b = a()
 xfitter.addAction(b)
 
 xfitter.run()
 
-from matplotlib.pyplot import plot,savefig
-plot(chi2l)
+from matplotlib.pyplot import *
+subplot(121)
+plot(chi2l[1:])
+subplot(122)
+plot(theo1[1:])
 savefig("chi2.pdf")
 
