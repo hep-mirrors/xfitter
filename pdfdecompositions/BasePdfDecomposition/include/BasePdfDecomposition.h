09e5ef3a7dfaaf8d310f60fa29cbdbd11231c8ae
Sasha Glazov <glazov@mail.desy.de>
2018-07-11 11:58:13 +0200

    Add BasePdfDecomposition class

---
+++ pdfdecompositions/BasePdfDecomposition/include/BasePdfDecomposition.h
@@ -0,0 +1,33 @@
+#pragma once
+
+
+#include <string>
+#include <map>
+
+/**
+  @class BasePdfDecomposition
+
+  @brief Base class to develop PDF decompositions
+
+  Contains methods to tranform from parameterisation to phyiscal basis
+
+  @version 0.1
+  @date 2018-07-11
+  */
+
+class BasePdfDecomposition {
+ public:
+
+  /// Default constructor. Name is the PDF name
+  BasePdfDecomposition(const std::string& inName){name = inName;}
+
+  /// Compute PDF in a physical base in LHAPDF format for given x and Q
+  virtual std::map<int,double> compute ( const double& x, const double& Q) const = 0; 
+
+ private:
+
+  /// Name of the PDF object (e.g. uv, dv ...)
+  std::string name;
+
+  /// Vector of parameters. Parameters are pointers to doubles
+};
