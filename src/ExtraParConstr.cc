8e8c1e52906b571a61c59e823ad732d026427e9a
Sasha Glazov <glazov@mail.desy.de>
2018-08-07 09:01:45 +0200

    Increase length of the name

--- src/ExtraParConstr.cc
+++ src/ExtraParConstr.cc
@@ -11,7 +11,7 @@ extern "C" {
 		#include "dimensions.h"
 		const int nExtraParamMax=NEXTRAPARAMMAX_C;
 		struct COMMON_ExtraPars_t {
-			char Names[nExtraParamMax][32];
+			char Names[nExtraParamMax][128];
 			double Value[nExtraParamMax], Step[nExtraParamMax], Min[nExtraParamMax], Max[nExtraParamMax], ConstrVal[nExtraParamMax], ConstrUnc[nExtraParamMax];
 			int iExtraParamMinuit[nExtraParamMax], nExtraParam;
 		};
@@ -54,15 +54,18 @@ extern "C" {
 
 		int firstpar=1;
 		for(int p=0; p<extrapars_.nExtraParam; p++) {
-			mnpout_(extrapars_.iExtraParamMinuit+p, parname, &par, &unc, &bound_l, &bound_h, &status, len);
+			mnpout_(extrapars_.iExtraParamMinuit+p, parname, &par, &unc, &bound_l, &bound_h, &status, len);		        
+
+
 			if(status<=0) continue;
+
 			if(firstpar) {
 				firstpar=0;
 				//printf("MINUIT EXTRA PARAMETERS:\n");
-				printf("%5s%20s%9s%9s%9s%9s%9s%9s\n", "NO", "NAME", "VALUE", "STEP", "LIM L", "LIM H","SHIFT","REDUCT");
+				printf("%5s%32s%9s%9s%9s%9s%9s%9s\n", "NO", "NAME", "VALUE", "STEP", "LIM L", "LIM H","SHIFT","REDUCT");
 			}
 			parname[std::string(parname).find(' ')]='\0';
-			printf("%5d%20s%9.4f%9.4f", extrapars_.iExtraParamMinuit[p], parname, par, unc);
+			printf("%5d%32s%9.4f%9.4f", extrapars_.iExtraParamMinuit[p], parname, par, unc);
 			if(bound_l!=0.0&&bound_h!=0.0) {
 				printf("%9.4f%9.4f", bound_l, bound_h);
 			}
