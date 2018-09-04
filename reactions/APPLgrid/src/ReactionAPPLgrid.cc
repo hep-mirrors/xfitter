17d86c53694ecdef469335e3f940a740517e5694
Ivan Novikov <novivanya@yandex.ru>
2018-09-04 13:59:43 +0200

    Allow each dataset to specify its own pair of evolutions for reaction APPLgrid

--- reactions/APPLgrid/src/ReactionAPPLgrid.cc
+++ reactions/APPLgrid/src/ReactionAPPLgrid.cc
@@ -18,17 +18,14 @@ extern "C" ReactionAPPLgrid* create() {
 function<void(double const&x,double const&Q,double*pdfs)>active_xfxQ_functions[2];
 void xfxWrapper0(const double&x,const double&Q,double*results){active_xfxQ_functions[0](x,Q,results);}
 void xfxWrapper1(const double&x,const double&Q,double*results){active_xfxQ_functions[1](x,Q,results);}
-ReactionAPPLgrid::ReactionAPPLgrid(){
-  evolutions[0]=nullptr;
-  evolutions[1]=nullptr;
-}
+ReactionAPPLgrid::ReactionAPPLgrid(){}
 ReactionAPPLgrid::~ReactionAPPLgrid(){}
  // Initialize at the start of the computation
 int ReactionAPPLgrid::initAtStart(const string &s){return 0;}
 
  // Initialize for a given dataset:
 void ReactionAPPLgrid::setDatasetParameters(int dataSetID, map<string,string> pars, map<string, double> parsDataset) {
-  //<DEBUG
+  /*DEBUG
   cerr<<"[DEBUG]ReactionAPPLgrid::setDatasetParameters(dataSetID="<<dataSetID<<",pars,parsDataset):\n"
       <<"pars={\n";
   for(map<string,string>::const_iterator it=pars.begin();it!=pars.end();++it){
@@ -40,69 +37,68 @@ void ReactionAPPLgrid::setDatasetParameters(int dataSetID, map<string,string> pa
     cerr<<"  \""<<it->first<<"\":"<<it->second<<'\n';
   }
   cerr<<"}"<<endl;
-  //DEBUG>
-	map<string,string>::iterator it;
-	// Get grid name:
+  DEBUG*/
+  DatasetData&data=dataset_data[dataSetID];
+  vector<TH1D*>&references=data.references;
+  BaseEvolution*(&evolutions)[2]=data.evolutions;
+  map<string,string>::iterator it;
+  // Get grid name:
   it=pars.find("GridName");
   if(it!=pars.end()){
     try{
       std::istringstream ss(it->second);
-       //std::cout << pars["GridName"] << '\n';
-       std::string token;
-       while(std::getline(ss, token, ','))
-       {
-         //std::cout << token << '\n';
-         std::shared_ptr<appl::grid>  g(new appl::grid(token));
-         g->trim();
-         _grids[dataSetID].push_back(g);
-         TFile file(token.c_str());
-         _references[dataSetID].push_back((TH1D*)file.Get("grid/reference"));
-         if(!_references[dataSetID].back())
-           hf_errlog(17033000, "W: no reference histogram grid/reference in " + token);
-         else
-           _references[dataSetID].back()->SetDirectory(0);
-       }
-     }
-     catch ( const std::exception& e ) {
-        std::string text = "F:Failed to read APPLgrid file(s) "+pars["GridName"];
-        hf_errlog_(17032802,text.c_str(),text.size());
-     }
-   }
-   else {
-     std::string text = "F:GridName must be specified for the Reaction APPLgrid";
-     hf_errlog_(17032801,text.c_str(),text.size());
-   }
+      std::string token;
+      while(std::getline(ss, token, ',')){
+        std::shared_ptr<appl::grid> g(new appl::grid(token));
+        g->trim();
+        data.grids.push_back(g);
+        TFile file(token.c_str());
+        references.push_back((TH1D*)file.Get("grid/reference"));
+        if(!references.back())
+          hf_errlog(17033000, "W: no reference histogram grid/reference in " + token);
+        else
+          references.back()->SetDirectory(0);
+      }
+    }
+    catch ( const std::exception& e ) {
+      std::string text = "F:Failed to read APPLgrid file(s) "+pars["GridName"];
+      hf_errlog_(17032802,text.c_str(),text.size());
+    }
+  }
+  else {
+    std::string text = "F:GridName must be specified for the Reaction APPLgrid";
+    hf_errlog_(17032801,text.c_str(),text.size());
+  }
 // Get pointers to evolutions
   for(int i=0;i<2;++i){
     string evolutionName="";
     it=pars.find("evolution"+to_string(i+1));
     if(it!=pars.end())evolutionName=it->second;//else default evolution name will be used
-    cerr<<"[DEBUG]APPLgrid: use evolution \""<<evolutionName<<'\"'<<endl;
+    //cerr<<"[DEBUG]APPLgrid: use evolution \""<<evolutionName<<'\"'<<endl;
     evolutions[i]=get_evolution(evolutionName);
   }
 // Determine order
-   int order = OrderMap( GetParamS("Order"));  // Global order
-   if (pars.find("Order") != pars.end() ) { // Local order
-       int localOrder = OrderMap( pars["Order"] );
-       order = localOrder>order ? order : localOrder;
-   }
-
-   _order[dataSetID] = order;
+  int order = OrderMap( GetParamS("Order"));  // Global order
+  if (pars.find("Order") != pars.end() ) { // Local order
+      int localOrder = OrderMap( pars["Order"] );
+      order = localOrder>order ? order : localOrder;
+  }
+  data.order=order;
 // Determine MuR and MuF.  Use default 
-  _muR[dataSetID] =  pars.find("muR") == pars.end() ? GetParam("muR") : stod(pars["muR"]);
-  _muF[dataSetID] =  pars.find("muF") == pars.end() ? GetParam("muF") : stod(pars["muF"]);
+  data.muR=pars.find("muR") == pars.end() ? GetParam("muR") : stod(pars["muR"]);
+  data.muF=pars.find("muF") == pars.end() ? GetParam("muF") : stod(pars["muF"]);
 
-  if (_muR[dataSetID] == 0) _muR[dataSetID] = 1.0; 
-  if (_muF[dataSetID] == 0) _muF[dataSetID] = 1.0;
+  if(data.muR==0)data.muR=1.0; 
+  if(data.muF==0)data.muF=1.0;
   // bin width normalisation (by default no rescaling)
-  _flagNorm[dataSetID] = false;
+  data.flagNorm=false;
   it = pars.find("norm");
   if (it != pars.end() )
   {
     if(stoi(it->second) == 0)
-      _flagNorm[dataSetID] = false;
+      data.flagNorm=false;
     else if(stoi(it->second) == 1)
-      _flagNorm[dataSetID] = true;
+      data.flagNorm=true;
     else
       hf_errlog(17102102, "F: unrecognised norm = " + it->second);
   }
@@ -111,13 +107,14 @@ void ReactionAPPLgrid::setDatasetParameters(int dataSetID, map<string,string> pa
   if (it != pars.end() )
   {
     if(stoi(it->second) == 0)
-      _flagUseReference[dataSetID] = false;
+      data.flagUseReference=false;
     else if(stoi(it->second) == 1)
     {
-      _flagUseReference[dataSetID] = true;
+      data.flagUseReference=true;
       // check that reference histogram is available
-      for(std::size_t i=0; i<_references[dataSetID].size(); i++)
-        if(!_references[dataSetID][i])
+      size_t endi=data.references.size();
+      for(size_t i=0;i<endi;++i)
+        if(!data.references[i])
           hf_errlog(17033000, "W: no reference histogram is available");
     }
     else
@@ -125,23 +122,24 @@ void ReactionAPPLgrid::setDatasetParameters(int dataSetID, map<string,string> pa
   }
   // CMS energy (by default the one used to create the grid is used)
   it = pars.find("energy");
-  for(unsigned int i = 0; i < _grids[dataSetID].size(); i++)
+  unsigned int endi=data.grids.size();
+  for(unsigned int i=0;i<endi;++i)
   {
     double eScale = 1.0;
     if (it != pars.end())
     {
-      if(_flagUseReference[dataSetID])
+      if(data.flagUseReference)
         hf_errlog(17110300, "W: can not apply energy scaling when using predictions from reference histogram");
       else
       {
-        double eStored = _grids[dataSetID][i]->getCMSScale();
+        double eStored=data.grids[i]->getCMSScale();
         if(eStored < 1e-3)
           hf_errlog(17110301, "W: can not apply energy scaling because stored getCMSScale = 0");
         else
-          eScale = _grids[dataSetID][i]->getCMSScale() / stof(it->second);
+          eScale = eStored/ stof(it->second);
       }
     }
-    _eScale[dataSetID].push_back(eScale);
+    data.eScale.push_back(eScale);
   }
 }
 
@@ -149,26 +147,35 @@ void ReactionAPPLgrid::setDatasetParameters(int dataSetID, map<string,string> pa
  // Main function to compute results at an iteration
 int ReactionAPPLgrid::compute(int dataSetID, valarray<double> &val, map<string, valarray<double> > &err) {
   // iterate over grids
+  DatasetData&data=dataset_data.at(dataSetID);
+  const int order=data.order;
+  const double muR=data.muR;
+  const double muF=data.muF;
+  BaseEvolution*(&evolutions)[2]=data.evolutions;
   unsigned int pos = 0;
-  for(unsigned int g = 0; g < _grids[dataSetID].size(); g++)
+  for(unsigned int g=0;g<data.grids.size();g++)
   {
-    auto grid = _grids[dataSetID][g];
+    auto grid=data.grids[g];
+    double eScale=data.eScale[g];
     std::vector<double> gridVals(grid->Nobs());
-    if(!_flagUseReference[dataSetID]){
+    if(!data.flagUseReference){
       //For some reason we do not take alphaS from evolutions?
       active_xfxQ_functions[0]=evolutions[0]->xfxQArray();
-      active_xfxQ_functions[1]=evolutions[1]->xfxQArray();
-      gridVals=grid->vconvolute(xfxWrapper0,xfxWrapper1,getAlphaS(),_order[dataSetID]-1,_muR[dataSetID],_muF[dataSetID],_eScale[dataSetID][g]);
-      //TODO: call specialized vconvolute when the two evolutions are the same
+      if(evolutions[0]==evolutions[1]){
+        gridVals=grid->vconvolute(xfxWrapper0,getAlphaS(),order-1,muR,muF,eScale);
+      }else{
+        active_xfxQ_functions[1]=evolutions[1]->xfxQArray();
+        gridVals=grid->vconvolute(xfxWrapper0,xfxWrapper1,getAlphaS(),order-1,muR,muF,eScale);
+      }
     }
     else
     {
       // use reference histogram
       for(std::size_t i=0; i<gridVals.size(); i++)
-        gridVals[i] = _references[dataSetID][g]->GetBinContent(i + 1);
+        gridVals[i]=data.references[g]->GetBinContent(i + 1);
     }
     // scale by bin width if requested
-    if(_flagNorm[dataSetID])
+    if(data.flagNorm)
       for (std::size_t i=0; i<gridVals.size(); i++)
         gridVals[i] *= grid->deltaobs(i);
 
@@ -177,9 +184,9 @@ int ReactionAPPLgrid::compute(int dataSetID, valarray<double> &val, map<string, 
     std::copy_n(gridVals.begin(), gridVals.size(), &val[pos]);
     pos += grid->Nobs();
   }
-  if(val.size()!=pos){
+  if(val.size()!=pos){//TODO: number of data points actually doesn't have to match grid size in some cases, so this check should be replaced by something else
     std::ostringstream s;
-    s<<"F: Number of data points ("<<val.size()<<") in dataset (ID="<<dataSetID<<") does not match total grid size ("<<pos<<")";
+    s<<"F: ReactionAPPLgrid: Number of data points ("<<val.size()<<") in dataset (ID="<<dataSetID<<") does not match total grid size ("<<pos<<")";
     hf_errlog(18072311,s.str().c_str());
   }
   return 0;
