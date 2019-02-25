2d8ef7c11953198d734eb345d70cb40d70542ed3
Oleksandr Zenaiev <oleksandr.zenaiev@cern.ch>
2019-02-25 19:26:10 +0100

    implemented ABMP parametrisation

---
+++ pdfparams/ABMPPdfParam/include/ABMPPdfParam.h
@@ -0,0 +1,29 @@
+
+#pragma once
+
+#include "BasePdfParam.h"
+
+/**
+  @class ABMPPdfParam 
+
+  @brief A class for ABMP pdf parameterisation
+
+  @version 0.1
+  @date 2019-02-25
+  */
+
+namespace xfitter{
+class ABMPPdfParam:public BasePdfParam{
+  public:
+    ABMPPdfParam(const std::string&inName):BasePdfParam(inName){}
+    //Evaluate xf(x) at given x with current parameters
+    virtual double operator()(double x)const override final;
+    // (Optional) compute moments:
+    // virtual double  moment(int nMoment=-1)const override final;
+    // (Optional) set moments:
+    // virtual void setMoment(int nMoment,double value)override final;
+    // (Optional)
+    //Initialize from a yaml node. Uses node[getName] as the basis
+    // virtual void initFromYaml(YAML::Node value)override final;
+};
+}
---
+++ pdfparams/ABMPPdfParam/src/ABMPPdfParam.cc
@@ -0,0 +1,25 @@
+ 
+/*
+   @file ABMPPdfParam.cc
+   @date 2019-02-25
+   @author AddPdfParam.py
+   Created by AddPdfParam.py on 2019-02-25
+*/
+
+#include "ABMPPdfParam.h"
+namespace xfitter{
+  //for dynamic loading
+  extern"C" ABMPPdfParam*create(const char*name){
+    return new ABMPPdfParam(name);
+  }
+  // Main function to compute PDF
+  double ABMPPdfParam::operator()(double x)const{
+    const int npar = getNPar();
+    if (npar<7) {
+      return NAN;
+    }
+  double power = (1 + *pars[3]) * (1 + *pars[4] * x + *pars[5] * x * x + *pars[6] * x * x * x);
+  double val = *pars[0] * pow(x, *pars[1]) * pow(1 - x, *pars[2]) * pow(x, power);
+  return val;
+}
+}
