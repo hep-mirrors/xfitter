0d3679c76229042ee7c2300a36557e0e287bac10
Sasha Glazov <glazov@mail.desy.de>
2019-04-08 20:08:45 +0200

    Check v14 and 15 of QCDNUM

--- src/init_theory.f
+++ src/init_theory.f
@@ -319,10 +319,10 @@ C Remove duplicates:
       call getint('mky0',mky)
       call getint('nwf0',nwf)
 *
-      if(ver.lt.170113 )then
+      if(ver.lt.170114 )then
          call HF_errlog(231020151, 'F: '//
      1   'Obsolete version of QCDNUM. '//
-     2   'Install version 17.01.13 or later.')
+     2   'Install version 17.01.14 or 17.01.15.')
       endif
       if(mxg.lt.5      )then
          call HF_errlog(231020152, 'F: '//
