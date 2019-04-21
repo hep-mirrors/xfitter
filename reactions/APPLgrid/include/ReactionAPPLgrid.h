4f1981dc7c49a7155029fa4d9d84aea9a20fa2f2
Ivan Novikov <novivanya@yandex.ru>
2019-04-21 15:31:53 +0200

    Rewrite reaction APPLgrid to use the new TermData interface

--- reactions/APPLgrid/include/ReactionAPPLgrid.h
+++ reactions/APPLgrid/include/ReactionAPPLgrid.h
@@ -1,44 +1,15 @@
-
 #pragma once
-
-#include "ReactionTheory.h"
-#include "appl_grid/appl_grid.h"
-#include <memory>
-#include "BaseEvolution.h"
-
+#include"ReactionTheory.h"
 /**
   @class' ReactionAPPLgrid
 
   @brief A wrapper class for APPLgrid reaction
-
-  Based on the ReactionTheory class. Reads options produces 3d cross section.
-
-  @version 0.1
-  @date 2017-03-28
   */
-struct DatasetData{
-  std::vector<std::shared_ptr<appl::grid> >grids;
-  int order;
-  double muR,muF; // !> renormalisation and factorisation scales
-  bool flagNorm; // !> if true, multiply by bin width
-  bool flagUseReference; // !> if true, prediction will be calculated from reference histogram (for tests and grids validation)
-  std::vector<TH1D*>  references;
-  std::vector<double> eScale; // !> CMS energy
-  double*scaleParameter=nullptr; // !> pointer to a minimization parameter which by which the predicted cross-section will be additionally multiplied. If this pointer is nullptr, no additional scaling is used.
-  xfitter::BaseEvolution*evolutions[2];
+class ReactionAPPLgrid:public ReactionTheory{
+public:
+  virtual string getReactionName()const{return"APPLgrid";};
+  virtual void initTerm(TermData*)override final;
+  virtual void freeTerm(TermData*)override final;
+  virtual void compute(TermData*,valarray<double>&,map<string,valarray<double> >&)override final;
 };
-class ReactionAPPLgrid : public ReactionTheory
-{
-  public:
-    ReactionAPPLgrid();
-    ~ReactionAPPLgrid();
-    virtual string getReactionName() const { return  "APPLgrid" ;};
-    int atStart(const string &);
-    virtual void setDatasetParameters( int dataSetID, map<string,string> pars, map<string,double> parsDataset) override ;
-    virtual int compute(int dataSetID, valarray<double> &val, map<string, valarray<double> > &err);
-  protected:
-    virtual int parseOptions(){ return 0;};
-  private:
-    map<int,DatasetData>dataset_data;
-};
-
+//NOTE: parameter "energy" is only read at start, once, and threrefore cannot be fitted
--- reactions/APPLgrid/src/ReactionAPPLgrid.cc
+++ reactions/APPLgrid/src/ReactionAPPLgrid.cc
@@ -9,200 +9,145 @@
 #include"TFile.h"
 #include"xfitter_pars.h"
 #include"xfitter_steer.h"
+#include"xfitter_cpp_base.h"
+#include"appl_grid/appl_grid.h"
+#include<memory>
+#include"BaseEvolution.h"
 using namespace std;
 using namespace xfitter;
- // the class factories
+struct GridData{
+  unique_ptr<appl::grid>grid;
+  TH1D*reference;//used if flagUseReference=true
+  double eScale;// !> CMS energy
+};
+struct DatasetData{
+  vector<GridData>grids;
+  int order;
+  const double*muR,*muF; // !> renormalisation and factorisation scales
+  bool flagNorm;  // !> if true, multiply by bin width
+  bool flagUseReference; // !> if true, prediction will be calculated from reference histogram (for tests and grids validation)
+};
+const double ONE=1;
 extern "C" ReactionAPPLgrid* create() {
   return new ReactionAPPLgrid();
 }
-function<void(double const&x,double const&Q,double*pdfs)>active_xfxQ_functions[2];
-void xfxWrapper0(const double&x,const double&Q,double*results){active_xfxQ_functions[0](x,Q,results);}
-void xfxWrapper1(const double&x,const double&Q,double*results){active_xfxQ_functions[1](x,Q,results);}
-ReactionAPPLgrid::ReactionAPPLgrid(){}
-ReactionAPPLgrid::~ReactionAPPLgrid(){}
- // Initialize at the start of the computation
-int ReactionAPPLgrid::atStart(const string &s){return 0;}
-
- // Initialize for a given dataset:
-void ReactionAPPLgrid::setDatasetParameters(int dataSetID, map<string,string> pars, map<string, double> parsDataset) {
-  DatasetData&data=dataset_data[dataSetID];
-  vector<TH1D*>&references=data.references;
-  BaseEvolution*(&evolutions)[2]=data.evolutions;
-  map<string,string>::iterator it;
-  // Get grid name:
-  it=pars.find("GridName");
-  if(it!=pars.end()){
-    try{
-      std::istringstream ss(it->second);
-      std::string token;
-      while(std::getline(ss, token, ',')){
-        std::shared_ptr<appl::grid> g(new appl::grid(token));
-        g->trim();
-        data.grids.push_back(g);
-        TFile file(token.c_str());
-        references.push_back((TH1D*)file.Get("grid/reference"));
-        if(!references.back())
-          hf_errlog(17033000, "W: no reference histogram grid/reference in " + token);
-        else
-          references.back()->SetDirectory(0);
-      }
-    }
-    catch ( const std::exception& e ) {
-      std::string text = "F:Failed to read APPLgrid file(s) "+pars["GridName"];
-      hf_errlog_(17032802,text.c_str(),text.size());
-    }
-  }
-  else {
-    std::string text = "F:GridName must be specified for the Reaction APPLgrid";
-    hf_errlog_(17032801,text.c_str(),text.size());
-  }
-// Get pointers to evolutions
-  for(int i=0;i<2;++i){
-    string evolutionName="";
-    it=pars.find("evolution"+to_string(i+1));
-    if(it!=pars.end())evolutionName=it->second;
-    else{
-      evolutionName=XFITTER_PARS::getDefaultEvolutionName();
-      string s="W: APPLgrid: using default evolution"+to_string(i+1)+"=\""+evolutionName+"\"";
-      hf_errlog(18112110+i,s);
-    }
-    evolutions[i]=get_evolution(evolutionName);
-  }
-// Determine order
-  int order = OrderMap( GetParamS("Order"));  // Global order
-  if (pars.find("Order") != pars.end() ) { // Local order
-      int localOrder = OrderMap( pars["Order"] );
-      order = localOrder>order ? order : localOrder;
-  }
-  data.order=order;
-// Determine MuR and MuF.  Use default
-  data.muR=pars.find("muR") == pars.end() ? GetParam("muR") : stod(pars["muR"]);
-  data.muF=pars.find("muF") == pars.end() ? GetParam("muF") : stod(pars["muF"]);
-
-  if(data.muR==0)data.muR=1.0;
-  if(data.muF==0)data.muF=1.0;
-  // bin width normalisation (by default no rescaling)
-  data.flagNorm=false;
-  it = pars.find("norm");
-  if (it != pars.end() )
-  {
-    if(stoi(it->second) == 0)
-      data.flagNorm=false;
-    else if(stoi(it->second) == 1)
-      data.flagNorm=true;
-    else
-      hf_errlog(17102102, "F: unrecognised norm = " + it->second);
-  }
-  // use reference histogram to calculate predictions (for tests or grids validation)
-  it = pars.find("useReference");
-  if (it != pars.end() )
-  {
-    if(stoi(it->second) == 0)
-      data.flagUseReference=false;
-    else if(stoi(it->second) == 1)
-    {
-      data.flagUseReference=true;
-      // check that reference histogram is available
-      size_t endi=data.references.size();
-      for(size_t i=0;i<endi;++i)
-        if(!data.references[i])
-          hf_errlog(17033000, "W: no reference histogram is available");
-    }
-    else
-      hf_errlog(17102102, "F: unrecognised useReference = " + it->second);
-  }
-  // CMS energy (by default the one used to create the grid is used)
-  it = pars.find("energy");
-  unsigned int endi=data.grids.size();
-  for(unsigned int i=0;i<endi;++i)
-  {
-    double eScale = 1.0;
-    if (it != pars.end())
-    {
-      if(data.flagUseReference)
-        hf_errlog(17110300, "W: can not apply energy scaling when using predictions from reference histogram");
+void ReactionAPPLgrid::initTerm(TermData*td){
+  DatasetData*data=new DatasetData;
+  td->reactionData=(void*)data;
+  //Split entries in GridName and load grids
+  string GridName=td->getParamS("GridName");
+  try{
+    std::istringstream ss(GridName);
+    std::string token;
+    while(std::getline(ss, token, ',')){
+      appl::grid*g=new appl::grid(token);
+      g->trim();
+      TFile file(token.c_str());
+      TH1D*reference=(TH1D*)file.Get("grid/reference");
+      if(!reference)
+        hf_errlog(17033000, "W: no reference histogram grid/reference in " + token);
       else
-      {
-        double eStored=data.grids[i]->getCMSScale();
-        if(eStored < 1e-3)
-          hf_errlog(17110301, "W: can not apply energy scaling because stored getCMSScale = 0");
-        else
-          eScale = eStored/ stof(it->second);
-      }
-    }
-    data.eScale.push_back(eScale);
-  }
-  // Additional scaling parameter
-  it=pars.find("scaleParameter");
-  if(it!=pars.end()){
-    try{
-      data.scaleParameter=XFITTER_PARS::gParameters.at(it->second);
-    }catch(out_of_range&ex){
-      cerr<<"[ERROR] Reaction APPLgrid for datasetID="<<dataSetID<<": unknown scaleParameter \""<<it->second<<"\""<<endl;
-      hf_errlog(18100800,"F: Unknown scaleParameter given to APPLgrid, see stderr");
+        reference->SetDirectory(0);//detach the reference histogram from the ROOT file
+      data->grids.push_back(GridData());
+      GridData&gd=data->grids.back();
+      gd.grid.reset(g);
+      gd.reference=reference;
     }
   }
-  // Check if there are any extra unknown parameters
-  {
-  const string parnames[]={"GridName","evolution1","evolution2","Order","muR","muF","useReference","scaleParameter"};
-  const size_t N=sizeof(parnames)/sizeof(string);
-  for(it=pars.begin();it!=pars.end();++it){
-    for(size_t i=0;i<N;++i){
-      if(it->first==parnames[i])goto cont;
-    }
-    cerr<<"[ERROR] Reaction APPLgrid for datasetID="<<dataSetID<<": unknown reaction parameter \""<<it->first<<"\""<<endl;
-    hf_errlog(18100801,"F: Unknown reaction parameter given to APPLgrid, see stderr");
-    cont:;
+  catch ( const std::exception& e ) {
+    hf_errlog(17032802,"F: Failed to read APPLgrid file(s) "+GridName);
   }
+  // Get Order
+  data->order=OrderMap(td->getParamS("Order"));
+  // Get MuR and MuF, or use 1.0 as default
+  if(td->hasParam("muR"))data->muR=td->getParamD("muR");
+  else data->muR=&ONE;
+  if(td->hasParam("muF"))data->muF=td->getParamD("muF");
+  else data->muF=&ONE;
+  // Get if should normalize by dividing by bin width (no by default)
+  data->flagNorm=false;
+  if(td->hasParam("norm")){
+    int norm=td->getParamI("norm");
+    if(norm==1){
+      data->flagNorm=true;
+    }else if(norm!=0){
+      hf_errlog(17102102, "F: unrecognised norm = " + norm);
+    }
+  }
+  size_t Ngrids=data->grids.size();
+  // Get if should use reference histogram to calculate predictions (for tests or grids validation) (no by default)
+  data->flagUseReference=false;
+  if(td->hasParam("useReference")){
+    int flag=td->getParamI("useReference");
+    if(flag==1){
+      data->flagUseReference=true;
+      // Make sure that the reference histogram is available
+      for(size_t i=0;i<Ngrids;++i)
+        if(!data->grids[i].reference)
+          hf_errlog(17033000, "F: no reference histogram is available");
+    }else if(flag!=0){
+      hf_errlog(17102102, "F: unrecognised useReference = " + to_string(flag));
+    }
+  }
+  // Get CMS energy (by default the one used to create the grid is used)
+  if(td->hasParam("energy")){
+    if(data->flagUseReference){
+      hf_errlog(17110300,"F: can not apply energy scaling when using predictions from reference histogram");
+    }
+    double energy=*td->getParamD("energy");
+    for(size_t i=0;i<Ngrids;++i){
+      double eScale=1;
+      double eStored=data->grids[i].grid->getCMSScale();
+      if(eStored<1e-3)hf_errlog(17110301, "W: can not apply energy scaling because stored getCMSScale = 0");
+      else eScale=eStored/energy;
+      data->grids[i].eScale=eScale;
+    }
+  }else{
+    for(size_t i=0;i<Ngrids;++i){
+      data->grids[i].eScale=1;
+    }
   }
 }
-
-
- // Main function to compute results at an iteration
-int ReactionAPPLgrid::compute(int dataSetID, valarray<double> &val, map<string, valarray<double> > &err) {
-  DatasetData&data=dataset_data.at(dataSetID);
+void ReactionAPPLgrid::freeTerm(TermData*td){
+  DatasetData*data=(DatasetData*)td->reactionData;
+  size_t Ngrids=data->grids.size();
+  for(size_t i=0;i<Ngrids;++i){
+    delete data->grids[i].reference;
+  }
+  delete data;
+}
+void ReactionAPPLgrid::compute(TermData*td,valarray<double>&val,map<string,valarray<double> >&err){
+  const DatasetData&data=*(DatasetData*)td->reactionData;
   const int order=data.order;
-  const double muR=data.muR;
-  const double muF=data.muF;
-  BaseEvolution*(&evolutions)[2]=data.evolutions;
+  const double muR=*data.muR;
+  const double muF=*data.muF;
   unsigned int pos = 0;
   for(unsigned int g=0;g<data.grids.size();g++)
   {
-    auto grid=data.grids[g];
-    double eScale=data.eScale[g];
+    appl::grid*grid=data.grids[g].grid.get();
+    double eScale=data.grids[g].eScale;
     std::vector<double> gridVals(grid->Nobs());
     if(!data.flagUseReference){
-      //For some reason we do not take alphaS from evolutions? --Ivan
-      active_xfxQ_functions[0]=evolutions[0]->xfxQArray();
-      if(evolutions[0]==evolutions[1]){
-        gridVals=grid->vconvolute(xfxWrapper0,getAlphaS(),order-1,muR,muF,eScale);
-      }else{
-        active_xfxQ_functions[1]=evolutions[1]->xfxQArray();
-        gridVals=grid->vconvolute(xfxWrapper0,xfxWrapper1,getAlphaS(),order-1,muR,muF,eScale);
-      }
+      td->actualizeWrappers();
+      gridVals=grid->vconvolute(PDF_xfxQ_wrapper,PDF_xfxQ_wrapper1,AlphaS_wrapper,order-1,muR,muF,eScale);
     }
     else
     {
       // use reference histogram
-      for(std::size_t i=0; i<gridVals.size(); i++)
-        gridVals[i]=data.references[g]->GetBinContent(i + 1);
+      TH1D*ref=data.grids[g].reference;
+      for(size_t i=0;i<gridVals.size();i++)
+        gridVals[i]=ref->GetBinContent(i + 1);
     }
     // scale by bin width if requested
     if(data.flagNorm)
-      for (std::size_t i=0; i<gridVals.size(); i++)
+      for(size_t i=0; i<gridVals.size(); i++)
         gridVals[i] *= grid->deltaobs(i);
-
-
     // insert values from this grid into output array
-    //val.resize(val.size() + grid->Nobs());
-    std::copy_n(gridVals.begin(), gridVals.size(), &val[pos]);
+    copy_n(gridVals.begin(), gridVals.size(), &val[pos]);
     pos += grid->Nobs();
   }
   if(val.size()!=pos){//TODO: number of data points actually doesn't have to match grid size in some cases, so this check should be replaced by something else
-    std::ostringstream s;
-    s<<"F: ReactionAPPLgrid: Number of data points ("<<val.size()<<") in dataset (ID="<<dataSetID<<") does not match total grid size ("<<pos<<")";
-    hf_errlog(18072311,s.str().c_str());
+    cerr<<"[ERROR] ReactionAPPLgrid: Number of data points ("<<val.size()<<") in term (ID="<<td->id<<") does not match total grid size ("<<pos<<")";
+    hf_errlog(18072311,"F: ReactionAPPLgrid: number of data points does not match total grid size, see stderr");
   }
-  if(data.scaleParameter)val*=*data.scaleParameter;
-  return 0;
 }
