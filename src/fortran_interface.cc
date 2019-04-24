0a74a8f0fc3fa9bb3c82761573a874c29116eb78
Ivan Novikov <novivanya@yandex.ru>
2019-04-24 20:56:33 +0200

    Update fortran interface to default evolution

--- src/fortran_interface.cc
+++ src/fortran_interface.cc
@@ -6,15 +6,15 @@
 using namespace std;
 //PDFs
 extern "C" void hf_get_pdfsq_(double const&x,double const&Q,double*pdfs){
-  xfitter::defaultEvolution->xfxQArray()(x,Q,pdfs);//returns through *pdfs
+  xfitter::defaultEvolution->xfxQarray(x,Q,pdfs);//returns through *pdfs
 }
 extern "C" void hf_get_pdfs_(double const&x,double const&Q2,double*pdfs){
-  xfitter::defaultEvolution->xfxQArray()(x,sqrt(Q2),pdfs);//returns through *pdfs
+  xfitter::defaultEvolution->xfxQarray(x,sqrt(Q2),pdfs);//returns through *pdfs
 }
 //alpha_s
 extern "C" double hf_get_alphasq_(double const&Q){
-  return xfitter::defaultEvolution->AlphaQCD()(Q);
+  return xfitter::defaultEvolution->getAlphaS(Q);
 }
 extern "C" double hf_get_alphas_(double const&Q2){
-  return xfitter::defaultEvolution->AlphaQCD()(sqrt(Q2));
+  return xfitter::defaultEvolution->getAlphaS(sqrt(Q2));
 }
