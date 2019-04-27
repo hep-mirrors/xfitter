b215c9af41800ef51592b2b5002c46e01a44e40a
Ivan Novikov <novivanya@yandex.ru>
2019-04-27 08:26:54 +0200

    Rewrite reaction KFactor using the new interface

--- reactions/KFactor/include/ReactionKFactor.h
+++ reactions/KFactor/include/ReactionKFactor.h
@@ -14,25 +14,12 @@
   @date 2017-10-28
   */
 
-class ReactionKFactor : public ReactionTheory
-{
-  public:
-    ReactionKFactor(){};
-
-    //    ~ReactionKFactor(){};
-    //    ~ReactionKFactor(const ReactionKFactor &){};
-    //    ReactionKFactor & operator =(const ReactionAKFactor &r){return *(new ReactionKFactor(r));};
-
-  public:
-    virtual string getReactionName() const { return  "KFactor" ;};
-    int atStart(const string &);
-    virtual void setDatasetParameters( int dataSetID, map<string,string> pars, map<string,double> parsDataset) override ;
-    virtual void initAtIteration() override;
-    virtual int compute(int dataSetID, valarray<double> &val, map<string, valarray<double> > &err);
-  protected:
-    virtual int parseOptions(){ return 0;};
-  private:
-    map<int, std::vector<double> > _values;
-    map<int, std::pair<std::string, double> > _parameterNames;
+class ReactionKFactor : public ReactionTheory{
+public:
+  ReactionKFactor(){};
+  virtual string getReactionName()const{return"KFactor";}
+  virtual void initTerm(TermData*)override final;
+  virtual void freeTerm(TermData*)override final;
+  virtual void compute(TermData*,valarray<double>&val,map<string,valarray<double> >&errors)override final;
 };
 
--- reactions/KFactor/src/ReactionKFactor.cc
+++ reactions/KFactor/src/ReactionKFactor.cc
@@ -10,54 +10,47 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include"hf_errlog.h"
+using namespace std;
 
 // the class factories
 extern "C" ReactionKFactor* create()
 {
   return new ReactionKFactor();
 }
-
-
-// Initialize at the start of the computation
-int ReactionKFactor::atStart(const string &s)
-{
-  return 0;
-}
-
+struct ReactionData{
+  valarray<double>_values;
+  const double*parameter=nullptr;
+  /* If parameter "Parameter" is given, KFactor returns column with all values equal to that parameter
+   * This parameter can be fitted
+   * If FileName is given, values are loaded from that file, and KFactor returns this constant column
+   * If DataColumn is given, the constant column is read from the datafile
+  */
+};
 // Initialisze for a given dataset:
-void ReactionKFactor::setDatasetParameters(int dataSetID, map<string,string> pars, map<string, double> parsDataset)
-{
+void ReactionKFactor::initTerm(TermData*td){
+  ReactionData*rd=new ReactionData;
+  td->reactionData=(void*)rd;
   // check if kfactors should be read from separate file
-  if (pars.find("FileName") != pars.end())
-  {
+  if(td->hasParam("FileName")){
     // file name to read kfactors
-    std::string fileName = pars["FileName"];
-
+    string fileName=td->getParamS("FileName");
     // requested column from file (by default 1st)
-    int column = 1;
-    if(pars.find("FileColumn") != pars.end())
-      column = atoi(pars["FileColumn"].c_str());
-
+    int column=1;
+    if(td->hasParam("FileColumn")){
+      column=td->getParamI("FileColumn");
+      if(column<1)hf_errlog(17102800, "F: wrong column = " + std::to_string(column));
+    }
     // requested starting line from file (by default 1st)
-    int lineStart = 1;
-    if(pars.find("FileLine") != pars.end())
-      lineStart = atoi(pars["FileLine"].c_str());
-
+    int lineStart=1;
+    if(td->hasParam("FileLine"))lineStart=td->getParamI("FileLine");
     // requested last line -> how many values to read (by default the same as number of data points)
-    // TODO: how to get number of data points?
-    // not very elegant way below
-    int np = _dsBins[dataSetID]->begin()->second.size();
-    int lineFinish = lineStart + np - 1;
-    if(pars.find("FileLineFinish") != pars.end())
-    {
-      lineFinish = atoi(pars["FileLineFinish"].c_str());
+    int np=td->getNbins();
+    int lineFinish=lineStart+np-1;
+    if(td->hasParam("FileLineFinish")){
+      lineFinish=td->getParamI("FileLineFinish");
       np = lineFinish - lineStart + 1;
     }
-
-    // check that the column is reasonable
-    if(column < 1)
-      hf_errlog(17102800, "F: wrong column = " + std::to_string(column));
-
     // open file
     std::ifstream file(fileName.c_str());
     string line;
@@ -68,12 +61,13 @@ void ReactionKFactor::setDatasetParameters(int dataSetID, map<string,string> par
     for(int l = 1; l < lineStart; l++)
       getline(file, line);
 
+    vector<double>_values;
     for(int p = 0; p < np; p++)
     //while (1)
     {
       // read new line
       getline(file, line);
-      if (true == file.eof()) break;
+      if(file.eof())break;
       if (line.at(0) == '#' ) continue; //ignore comments
       line.erase(line.find_last_not_of(" \n\r\t")+1); // trim trailing whitespaces
       std::stringstream sline(line);
@@ -100,48 +94,34 @@ void ReactionKFactor::setDatasetParameters(int dataSetID, map<string,string> par
         sline >> val;
 
       // store kfactor value
-      _values[dataSetID].push_back(val);
+      _values.push_back(val);
     }
     file.close();
+    rd->_values=valarray<double>(_values.data(),_values.size());
   }
-  // check if kfactors should be read from data file (soecifying column is mandatory)
-  else if (pars.find("DataColumn") != pars.end())
+  // check if kfactors should be read from data file (specifying column is mandatory)
+  else if(td->hasParam("DataColumn"))
   {
-    std::string columnName = pars["DataColumn"];
-    valarray<double>* vals = this->GetBinValues(dataSetID, columnName);
-    if(!vals)
-      hf_errlog(17102803, "F: no column = " + columnName + " in data file");
-    // store kfactor values
-    _values[dataSetID].resize(vals->size());
-    _values[dataSetID].assign(std::begin(*vals), std::end(*vals));
+    rd->_values=td->getBinColumn(td->getParamS("DataColumn"));
   }
   // check if kfactor is a parameter (possibly free); the value of this parameter will be used for all bins in data set
-  else  if (pars.find("Parameter") != pars.end())
+  else if(td->hasParam("Parameter"))
   {
-    _parameterNames[dataSetID] = std::make_pair(pars["Parameter"], 0.0);
+    rd->parameter=td->getParamD("Parameter");
   }
   else
     hf_errlog(17102804, "F: FileName or DataColumn or Parameter must be provided for KFactor");
 }
-
-void ReactionKFactor::initAtIteration() {
-  for(auto& it : _parameterNames)
-    it.second.second = GetParam(it.second.first);
-}
-
+void ReactionKFactor::freeTerm(TermData*td){delete(ReactionData*)td->reactionData;}
 // Main function to compute results at an iteration
-int ReactionKFactor::compute(int dataSetID, valarray<double> &val, map<string, valarray<double> > &err)
+void ReactionKFactor::compute(TermData*td, valarray<double> &val, map<string, valarray<double> > &err)
 {
-  const auto& it = _parameterNames.find(dataSetID);
-  if(it != _parameterNames.end())
-  {
-    // kfactor given as a fit parameter read in initAtIteration()
-    int np = _dsBins[dataSetID]->begin()->second.size(); // number of data points
-    val = std::valarray<double>(it->second.second, np);
+  ReactionData*rd=(ReactionData*)td->reactionData;
+  if(rd->parameter){
+    double parval=*rd->parameter;
+    for(auto&v:val)v=parval;
+  }else{ // kfactor is constant value read in setDatasetParameters()
+    val=rd->_values;
   }
-  else
-    // kfactor is constant value read in setDatasetParameters()
-    val = std::valarray<double>(_values[dataSetID].data(), _values[dataSetID].size());
-  return 0;
 }
 
