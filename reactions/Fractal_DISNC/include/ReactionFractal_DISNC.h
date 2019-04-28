51fc90b0e5852eba8141c7db55712e79e48e7784
Ivan Novikov <novivanya@yandex.ru>
2019-04-28 12:44:34 +0200

    Rewrite reaction Fractal_DISNC using TermData

--- reactions/Fractal_DISNC/include/ReactionFractal_DISNC.h
+++ reactions/Fractal_DISNC/include/ReactionFractal_DISNC.h
@@ -14,20 +14,9 @@
   @date 2017-05-15
   */
 
-class ReactionFractal_DISNC : public ReactionTheory
-{
-  public:
-    ReactionFractal_DISNC(){};
-
-//    ~ReactionFractal_DISNC(){};
-//    ~ReactionFractal_DISNC(const ReactionFractal_DISNC &){};
-//    ReactionFractal_DISNC & operator =(const ReactionAFractal_DISNC &r){return *(new ReactionFractal_DISNC(r));};
-
-  public:
-    virtual string getReactionName() const { return  "Fractal_DISNC" ;};
-    int atStart(const string &){return 0;};
-    virtual int compute(int dataSetID, valarray<double> &val, map<string, valarray<double> > &err);
-  protected:
-//    virtual int parseOptions(){ return 0;};
+class ReactionFractal_DISNC:public ReactionTheory{
+public:
+  virtual string getReactionName()const{return"Fractal_DISNC";};
+  virtual void compute(TermData*,valarray<double>&,map<string,valarray<double> >&)override final;
 };
 
