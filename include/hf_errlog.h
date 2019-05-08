326016b69f7e6a069d0ffb9412d3d93b1b4fbbe1
Sasha Glazov <glazov@mail.desy.de>
2019-05-08 14:07:22 +0200

    do not forget to generate Makefiles for new pdf params and decompositions

--- include/hf_errlog.h
+++ include/hf_errlog.h
@@ -1,3 +1,4 @@
 #pragma once
 #include<string>
+/// Report errors and messages with different level of severity (I:, W:, S:). S: stops the execution
 void hf_errlog(int id,const std::string&message);
