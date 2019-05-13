16d8501d0e8acd064bd4c4c0e6fee477f37d92ff
Oleksandr Zenaiev <oleksandr.zenaiev@cern.ch>
2019-05-13 21:15:56 +0200

    src/store_output.f

--- src/fortran_interface.cc
+++ src/fortran_interface.cc
@@ -6,19 +6,15 @@
 using namespace std;
 //PDFs
 extern "C" void hf_get_pdfsq_(double const&x,double const&Q,double*pdfs){
-  //printf("hf_get_pdfsq_ should not be used: use pdf_xfxq_wrapper_\n");
   xfitter::defaultEvolution->xfxQarray(x,Q,pdfs);//returns through *pdfs
 }
 extern "C" void hf_get_pdfs_(double const&x,double const&Q2,double*pdfs){
-  //printf("hf_get_pdfs_ should not be used: use pdf_xfxq_wrapper_\n");
   xfitter::defaultEvolution->xfxQarray(x,sqrt(Q2),pdfs);//returns through *pdfs
 }
 //alpha_s
 extern "C" double hf_get_alphasq_(double const&Q){
-  //printf("hf_get_alphasq_ should not be used: use alphas_wrapper_\n");
   return xfitter::defaultEvolution->getAlphaS(Q);
 }
 extern "C" double hf_get_alphas_(double const&Q2){
-  //printf("hf_get_alphas_ should not be used: use alphas_wrapper_\n");
   return xfitter::defaultEvolution->getAlphaS(sqrt(Q2));
 }
