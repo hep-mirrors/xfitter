12eaf261eb16460e51d4e980bc2c6e4f08294513
Oleksandr Zenaiev <oleksandr.zenaiev@cern.ch>
2019-07-23 17:24:07 +0200

    omit ceres test by default

--- tools/test.sh
+++ tools/test.sh
@@ -2,6 +2,7 @@
 
 # list of tests to omit (if commented out, no tests are omitted)
 #omitTests=('ZMVFNS-fit' 'profilerLHAPDF') # these are two slow tests, skipping them will save ~15min
+omitTests=('ceresZMVFNSfastChi2')
 
 # xfitter binary
 xfitter=`pwd`/'bin/xfitter'
