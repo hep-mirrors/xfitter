06ac88daf117815d2af300c958f8948b24b5bbf4
Ivan Novikov <ivan.novikov@desy.de>
2020-02-25 15:11:43 +0100

    Modify FastNLO to use the new reaction interface

--- reactions/fastNLO/include/ReactionfastNLO.h
+++ reactions/fastNLO/include/ReactionfastNLO.h
@@ -9,6 +9,7 @@
 #include <vector>
 #include "BaseEvolution.h"
 #include "xfitter_steer.h"
+using xfitter::BaseEvolution;
 
 /**
   @class' ReactionfastNLO
@@ -22,38 +23,30 @@
   */
 
 class fastNLOReaction : public fastNLOReader {
-  public:
-    fastNLOReaction(string name, ReactionTheory* reaction) : fastNLOReader(name) {fReaction=reaction;};
-  protected:
-    fastNLOReaction(string name) : fastNLOReader(name) {}; // not public!
-    ReactionTheory* fReaction=NULL;
-    double EvolveAlphas(double q ) const {
-       return xfitter::defaultEvolution->getAlphaS(q);
-       //return alphas_wrapper_(q);  //old fortran
-    } //!< provide alpha_s to fastNLO
-    bool InitPDF() { return true;}; //!< required by fastNLO
-    vector<double> GetXFX(double xp, double muf) const {
-       std::vector<double> pdfV(14);
-       // pdf_xfxq_wrapper_(xp, muf, &pdfV[0]); // old fortran wrapper
-       xfitter::defaultEvolution->xfxQarray(xp,muf,&pdfV[0]);
-       return pdfV;
-    }//!< provide PDFs to fastNLO
+ public:
+  fastNLOReaction(string name, BaseEvolution* ev) : fastNLOReader(name) {evolution=ev;};
+ protected:
+  fastNLOReaction(string name) : fastNLOReader(name) {}; // not public!
+  BaseEvolution* evolution=nullptr;
+  double EvolveAlphas(double q ) const {
+    return evolution->getAlphaS(q);
+  } //!< provide alpha_s to fastNLO
+  bool InitPDF() { return true;}; //!< required by fastNLO
+  vector<double> GetXFX(double xp, double muf) const {
+    vector<double> pdfV(14);
+    evolution->xfxQarray(xp,muf,pdfV.data());
+    return pdfV;
+  }//!< provide PDFs to fastNLO
 };
 
 class ReactionfastNLO : public ReactionTheory {
   public:
     ReactionfastNLO(){};
-
-    //    ~ReactionfastNLO(){};
-    //    ~ReactionfastNLO(const ReactionfastNLO &){};
-    //    ReactionfastNLO & operator =(const ReactionAfastNLO &r){return *(new ReactionfastNLO(r));};
-
   public:
     virtual string getReactionName() const { return  "fastNLO" ;};
     virtual void initTerm(TermData* td) override final;
+    virtual void freeTerm(TermData* td) override final;
     virtual void compute(TermData* td, valarray<double> &val, map<string, valarray<double> > &err);
-  protected:
-    std::map<int,std::vector<fastNLOReaction*> > ffnlo;
 };
 
 #endif
