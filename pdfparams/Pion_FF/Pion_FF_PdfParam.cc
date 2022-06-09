098f3e860d59849a2901567be760c541de0f4d88
Hamed <h.abdolmalki@gmail.com>
2022-06-09 14:45:11 +0430

    fixed the pdf_parm problem

--- pdfparams/Pion_FF/Pion_FF_PdfParam.cc
+++ pdfparams/Pion_FF/Pion_FF_PdfParam.cc
@@ -22,26 +22,37 @@ void Pion_FF_PdfParam::atStart(){
   atIteration();
 }
 void Pion_FF_PdfParam::atIteration(){
-      cout<<"########hello hamed PDF_par01#############";
+//      cout<<"########hello hamed PDF_par01#############";
   updateNormalization();
 }
 void Pion_FF_PdfParam::updateNormalization(){
   //norm=A/beta(B+1,C+1)
   const double b=*pars[1]+2;
   const double c=*pars[2]+1;
-  norm=(*pars[0])*exp(-lgamma(b)-lgamma(c)+lgamma(b+c));
+  const double d=*pars[2]+*pars[4]+1;
+   //cout<<"######hello hamed PDF_par01#####"<< *pars[1] <<"##"<< *pars[2] <<"##"<< *pars[3];
+
+   //  cout<<"########hello hamed PDF_par1########"<< *pars[2] <<"##"<< *pars[4] <<"##"<< d;
+  //norm=(*pars[0])/(exp(lgamma(b)+lgamma(c)-lgamma(b+c)) + *pars[3]*(exp(lgamma(b)+lgamma(d)-lgamma(b+d))));
+  norm = 1;
+//cout<<"########hello hamed PDF_par1##########"<< norm<<"***";
+
 }
 double Pion_FF_PdfParam::operator()(double x)const{
   const unsigned int npar=getNPar();
-  double power=pow(x,(*pars[1])+1)*pow((1-x),(*pars[2]));
+  double power=pow(x,(*pars[1]))*pow((1-x),(*pars[2]));
   double poly = 1;
   double xx = 1;
-      cout<<"########hello hamed PDF_par1#############";
+             // cout<<"########hello hamed PDF_par2##########"<< poly<<"***";
+
 //  for (unsigned int i = 3; i<npar; i++) {
 //    xx *= x;
 //    poly+=(*pars[i])*xx;
-    poly= 1 + (*pars[3])*pow(x , 0.5) + (*pars[4])*pow((1-x) , (*pars[5])) ;
-      cout<<"########hello hamed PDF_par2#############";
+   // poly= 1 - (*pars[3])*pow((1-x) , (*pars[4])) ;
+    poly = 2;
+      //    cout<<"########hello hamed PDF_par2##########"<< norm<<"###"<<poly<<"###"<<power<<"***";
+
+
 //  }
   return norm*power*poly;
 }
