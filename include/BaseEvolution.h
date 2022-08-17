a008cb900689e9cff72799eff1d3ab267b0f91d5
Stefano Camarda <stefano.camarda@cern.ch>
2022-08-18 01:19:51 +0200

    -1 as default for getPropertyD

--- include/BaseEvolution.h
+++ include/BaseEvolution.h
@@ -103,7 +103,7 @@ namespace xfitter
     virtual double  getPropertyD(std::string const& propertyName ) const { return 0.; }
 
     /// Get generic property of the evolution with default value and no error
-    virtual double  getPropertyD(std::string const& propertyName, double defval ) const { return 0.; }
+    virtual double  getPropertyD(std::string const& propertyName, double defval ) const { return -1.; }
 
     /// Get class name, can be used to verify that the correct concrete class is being used
     virtual const char*getClassName()const=0;
