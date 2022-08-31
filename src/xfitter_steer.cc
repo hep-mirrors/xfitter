21a241e369c80bf1648c639850fbb79e5bc26186
Stefano Camarda <stefano.camarda@cern.ch>
2022-08-31 23:50:49 +0200

    treat covariance matrix errors as E:

--- src/xfitter_steer.cc
+++ src/xfitter_steer.cc
@@ -100,10 +100,10 @@ void report_convergence_status_(){
       hf_errlog(16042801,"I: No minimization has run");
       break;
     case ConvergenceStatus::INACCURATE:
-      hf_errlog(16042803,"S: Error matrix not accurate");
+      hf_errlog(16042803,"E: Error matrix not accurate");
       break;
     case ConvergenceStatus::FORCED_POSITIVE:
-      hf_errlog(16042804,"W: Error matrix forced positive");
+      hf_errlog(16042804,"E: Error matrix forced positive");
       break;
     case ConvergenceStatus::SUCCESS:
       hf_errlog(16042802,"I: Fit converged");
