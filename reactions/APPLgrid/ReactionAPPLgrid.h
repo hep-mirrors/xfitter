23aa9be94cf5450ad3e3a2bcbf89873be76fba36
Sasha Glazov <glazov@mail.desy.de>
2023-06-05 12:15:02 +0200

    Synchronize APPLgrid CKM with xFitter

--- reactions/APPLgrid/ReactionAPPLgrid.h
+++ reactions/APPLgrid/ReactionAPPLgrid.h
@@ -10,6 +10,9 @@ public:
   virtual string getReactionName() const override {return"APPLgrid";};
   virtual void initTerm(TermData*) override final;
   virtual void freeTerm(TermData*) override final;
+  virtual void atIteration() override final;
   virtual void compute(TermData*,valarray<double>&,map<string,valarray<double> >&) override final;
+private:
+  std::vector< vector<double> > _ckm{ {1,0,0}, {0.,1.,0}, {0.,0.,1} };
 };
 //NOTE: parameter "energy" is only read at start, once, and therefore cannot be fitted
