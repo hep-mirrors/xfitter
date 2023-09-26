381289d9043343502295dedba0c95951958b1b7d
Toni Mäkelä <toni.makela@cern.ch>
2023-09-26 11:47:09 +0200

    Omitting the fpf-pseudodata / pineappl example in test.sh, as it requires fpf pseudodata

--- tools/test.sh
+++ tools/test.sh
@@ -3,7 +3,7 @@
 # list of tests to omit (if commented out, no tests are omitted)
 #omitTests=('ZMVFNS-fit' 'profilerLHAPDF') # these are two slow tests, skipping them will save ~15min
 #omitTests=('ceresZMVFNSfastChi2' 'chi2scanMTOP')
-omitTests=('profilerCIJET' 'ZPT' 'ZMVFNS-fit' 'scanmin' 'CERES-fit' 'CERES-parallel' 'CERES-Chebyschev') 
+omitTests=('fpf-pseudodata' 'profilerCIJET' 'ZPT' 'ZMVFNS-fit' 'scanmin' 'CERES-fit' 'CERES-parallel' 'CERES-Chebyschev') 
 
 install_dir=$(pwd)
 # xfitter binary
