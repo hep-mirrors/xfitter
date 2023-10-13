7a5979497d70229aa23af3c8353bf006aff40730
Oleksandr Zenaiev <oleksandr.zenaiev@gmail.com>
2023-10-13 18:35:36 +0200

    refactored TMC

--- reactions/FFABM_DISNC/ReactionFFABM_DISNC.cc
+++ reactions/FFABM_DISNC/ReactionFFABM_DISNC.cc
@@ -212,72 +212,14 @@ void ReactionFFABM_DISNC::calcF2FL(unsigned dataSetID)
                       f2, fl, f3, f2c, flc, f3c, f2b, flb, f3b,
                       ncflag, charge, polarity, *_sin2thwPtr, cos2thw, *_mzPtr);
         if(_flag_tmc) {
+          //printf("Q2,x = %6.1f x = %6.4f  c,b/l[%%] = %+5.2f[%+5.2f] %+5.2f[%+5.2f]\n", q2[i], x[i], f2c/f2*100, flc/fl*100, f2b/f2*100, flb/fl*100);
           // target mass corrections
-          double mn = 0.938272;
-          double gam = sqrt(1+4*x[i]*x[i]*mn*mn/q2[i]);
-          double xi = 2*x[i]/(1+gam);
-          auto integrate = [](double xip, void* params) {
-            const integration_params& integrationParams = *(integration_params*)params;
-            double f2(0), f2b(0), f2c(0), fl(0), flc(0), flb(0), f3(0), f3b(0), f3c(0);
-            sf_abkm_wrap_(xip, integrationParams.q2[integrationParams.i],
-                        f2, fl, f3, f2c, flc, f3c, f2b, flb, f3b,
-                        integrationParams.ncflag, integrationParams.charge, integrationParams.polarity, *integrationParams._sin2thwPtr, integrationParams.cos2thw, *integrationParams._mzPtr);
-            if (integrationParams.flag_calc_fl == 0) {
-              return f2/xip/xip;
-            }
-            else {
-              return fl/xip/xip;
-            }
-          };
-          integration_params pars;
-          pars.q2 = q2;
-          pars.i = i;
-          pars.ncflag = ncflag;
-          pars.charge = charge;
-          pars.polarity = polarity;
-          pars.cos2thw = cos2thw;
-          pars._sin2thwPtr = _sin2thwPtr;
-          pars._mzPtr = _mzPtr;
-          pars.flag_calc_fl = 0;
-          // numerical integration
-          gsl_function F;
-          F.function = integrate;
-          F.params = &pars;
-          size_t alloc_space = 1000;
-          gsl_integration_workspace * w = gsl_integration_workspace_alloc(alloc_space);
-          double epsabs = 0;
-          double epsrel = 1e-3;
-          int key_param = 6;
-          double result, error;
-          gsl_integration_qag (&F, xi, 1.0, epsabs, epsrel, alloc_space, key_param, w, &result, &error);
-          gsl_integration_workspace_free (w);
-          // Simpson 3/8 integration
-          double a = xi;
-          //double b = log10(1/xi);
-          double b = a*5;
-          if (b > 0.999) 
-            b = 0.999;
-          double sim38 = (b-a)/8.*(integrate(a, &pars)+3*integrate((2*a+b)/3., &pars)+3*integrate((a+2*b)/3., &pars)+integrate(b, &pars));
-          //printf("%f %f %f %f\n", integrate(a, &pars), integrate((2*a+b)/3., &pars), integrate((a+2*b)/3., &pars), integrate(b, &pars));
-          double I = result;
-          //I = sim38;
-          double f20 = f2;
-          //f2 = x[i]*x[i]/xi/xi/gam/gam/gam*f2 + 6*x[i]*x[i]*x[i]*mn*mn/q2[i]/gam/gam/gam/gam*I;
-          double f2_at_xi = integrate(xi, &pars)*xi*xi;
-          printf("f2: %f %f\n", f2, f2_at_xi);
-          f2 = x[i]*x[i]/xi/xi/gam/gam/gam*f2_at_xi + 6*x[i]*x[i]*x[i]*mn*mn/q2[i]/gam/gam/gam/gam*I;
-          //double ft = f2 - fl;
-          //ft = x[i]*x[i]/xi/xi/gam*ft + 2*x[i]*x[i]*x[i]*mn*mn/q2[i]/gam/gam*I;
-          pars.flag_calc_fl = 1;
-          double fl_at_xi = integrate(xi, &pars)*xi*xi;
-          double ft_at_xi = f2_at_xi - fl_at_xi;
-          double ft = x[i]*x[i]/xi/xi/gam*ft_at_xi + 2*x[i]*x[i]*x[i]*mn*mn/q2[i]/gam/gam*I;
-          fl = f2 - ft;
-          //fl = fl + x[i]*x[i]/gam/gam*(1-gam*gam)*f2_at_xi/xi/xi+mn*mn*x[i]*x[i]*x[i]/q2[i]/gam/gam/gam/gam*I;
-          double fl0 = fl;
-          printf("SZ [x,q2 = %f %f] result +- error = %f +- %f [%f] sim38 = %f [%f] [%f]\n", x[i], q2[i], result, error, error/result, sim38, sim38/result-1, f2/f20-1);
-          if (fabs(f20/f2-1) >  maxdiff)
-            maxdiff = fabs(f20/f2-1);
+          auto diff = apply_tmc(f2, fl, 1, q2, x, ncflag, charge, polarity, cos2thw, i);
+          if (fabs(diff) >  maxdiff)
+            maxdiff = fabs(diff);
+          //auto diff_c = apply_tmc(f2c, flc, 2, q2, x, ncflag, charge, polarity, cos2thw, i);
+          //auto diff_b = apply_tmc(f2b, flb, 3, q2, x, ncflag, charge, polarity, cos2thw, i);
+          //printf("TMC Q2,x = %6.1f x = %6.4f l,c,b[%%] = %+5.1f %+5.1f %+5.1f\n", q2[i], x[i], diff*100, diff_c*100, diff_b*100);
         }
         if (_flag_ht[td->id]) {
           double q02 = 1.;
@@ -333,4 +275,87 @@ void ReactionFFABM_DISNC::xF3 BASE_PARS
 {
   calcF2FL(td->id);
   val = _f3abm[td->id];
+}
+
+double ReactionFFABM_DISNC::apply_tmc(double& f2, double& fl, const int flag_flavour, const std::valarray<double>& q2, const std::valarray<double>& x,
+    const int ncflag, const int charge, const double polarity, const double cos2thw, const size_t i) {
+  double mn = 0.938272;
+  double gam = sqrt(1+4*x[i]*x[i]*mn*mn/q2[i]);
+  double xi = 2*x[i]/(1+gam);
+  auto integrate = [](double xip, void* params) {
+    const integration_params& integrationParams = *(integration_params*)params;
+    double f2(0), f2b(0), f2c(0), fl(0), flc(0), flb(0), f3(0), f3b(0), f3c(0);
+    sf_abkm_wrap_(xip, integrationParams.q2[integrationParams.i],
+                f2, fl, f3, f2c, flc, f3c, f2b, flb, f3b,
+                integrationParams.ncflag, integrationParams.charge, integrationParams.polarity, *integrationParams._sin2thwPtr, integrationParams.cos2thw, *integrationParams._mzPtr);
+    if (integrationParams.flag_calc_fl == 0) {
+      if (integrationParams.flag_flavour == 1)
+        return f2/xip/xip;
+      else if (integrationParams.flag_flavour == 2)
+        return f2c/xip/xip;
+      else if (integrationParams.flag_flavour == 3)
+        return f2b/xip/xip;
+      else
+        return 0.;
+    }
+    else {
+      if (integrationParams.flag_flavour == 1)
+        return fl/xip/xip;
+      else if (integrationParams.flag_flavour == 2)
+        return flc/xip/xip;
+      else if (integrationParams.flag_flavour == 3)
+        return flb/xip/xip;
+      else
+        return 0.;
+    }
+  };
+  integration_params pars;
+  pars.q2 = q2;
+  pars.i = i;
+  pars.ncflag = ncflag;
+  pars.charge = charge;
+  pars.polarity = polarity;
+  pars.cos2thw = cos2thw;
+  pars._sin2thwPtr = _sin2thwPtr;
+  pars._mzPtr = _mzPtr;
+  pars.flag_calc_fl = 0;
+  pars.flag_flavour = flag_flavour;
+  // numerical integration
+  gsl_function F;
+  F.function = integrate;
+  F.params = &pars;
+  size_t alloc_space = 1000;
+  gsl_integration_workspace * w = gsl_integration_workspace_alloc(alloc_space);
+  double epsabs = 0;
+  double epsrel = 1e-3;
+  int key_param = 6;
+  double result, error;
+  gsl_integration_qag (&F, xi, 1.0, epsabs, epsrel, alloc_space, key_param, w, &result, &error);
+  gsl_integration_workspace_free (w);
+  // Simpson 3/8 integration
+  double a = xi;
+  //double b = log10(1/xi);
+  double b = a*5;
+  if (b > 0.999) 
+    b = 0.999;
+  double sim38 = (b-a)/8.*(integrate(a, &pars)+3*integrate((2*a+b)/3., &pars)+3*integrate((a+2*b)/3., &pars)+integrate(b, &pars));
+  //printf("%f %f %f %f\n", integrate(a, &pars), integrate((2*a+b)/3., &pars), integrate((a+2*b)/3., &pars), integrate(b, &pars));
+  double I = result;
+  //I = sim38;
+  double f20 = f2;
+  //f2 = x[i]*x[i]/xi/xi/gam/gam/gam*f2 + 6*x[i]*x[i]*x[i]*mn*mn/q2[i]/gam/gam/gam/gam*I;
+  double f2_at_xi = integrate(xi, &pars)*xi*xi;
+  //printf("f2: %f %f\n", f2, f2_at_xi);
+  f2 = x[i]*x[i]/xi/xi/gam/gam/gam*f2_at_xi + 6*x[i]*x[i]*x[i]*mn*mn/q2[i]/gam/gam/gam/gam*I;
+  //double ft = f2 - fl;
+  //ft = x[i]*x[i]/xi/xi/gam*ft + 2*x[i]*x[i]*x[i]*mn*mn/q2[i]/gam/gam*I;
+  pars.flag_calc_fl = 1;
+  double fl_at_xi = integrate(xi, &pars)*xi*xi;
+  double ft_at_xi = f2_at_xi - fl_at_xi;
+  double ft = x[i]*x[i]/xi/xi/gam*ft_at_xi + 2*x[i]*x[i]*x[i]*mn*mn/q2[i]/gam/gam*I;
+  fl = f2 - ft;
+  //fl = fl + x[i]*x[i]/gam/gam*(1-gam*gam)*f2_at_xi/xi/xi+mn*mn*x[i]*x[i]*x[i]/q2[i]/gam/gam/gam/gam*I;
+  double fl0 = fl;
+  printf("SZ [x,q2 = %f %f] result +- error = %f +- %f [%f] sim38 = %f [%f] [%f]\n", x[i], q2[i], result, error, error/result, sim38, sim38/result-1, f2/f20-1);
+  return f20/f2-1;
 }
