c4fe08d33c347adbce5fe1a5603c3ed1ec23c166
Oleksandr Zenaiev <oleksandr.zenaiev@gmail.com>
2025-02-06 12:46:08 +0100

    better access to alphas_Q0 in QCDNUM

--- evolutions/QCDNUM/EvolutionQCDNUM.cc
+++ evolutions/QCDNUM/EvolutionQCDNUM.cc
@@ -120,10 +120,10 @@ namespace xfitter {
     _Q0 = *XFITTER_PARS::getParamD((yQCDNUM["Q0"]) ? yQCDNUM["Q0"].as<string>() : "Q0");
     double q20 = _Q0 * _Q0;
     _alphas = XFITTER_PARS::getParamD((yQCDNUM["alphas"]) ? yQCDNUM["alphas"].as<string>() : "alphas");
-    try {
-      _alphas_q0 = XFITTER_PARS::getParamD((yQCDNUM["alphas_Q0"]) ? yQCDNUM["alphas_Q0"].as<string>() : "alphas_Q0");
+    if (yQCDNUM["alphas_Q0"]) {
+      _alphas_q0 = XFITTER_PARS::getParamD(yQCDNUM["alphas_Q0"].as<string>());
     }
-    catch(std::out_of_range&ex){
+    else {
       _alphas_q0 = nullptr;
     }
     if (!_alphas_q0) {
