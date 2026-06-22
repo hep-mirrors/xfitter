577f88bfd6b8546b2bb75a6d25a7d406ab9bef0e
Oleksandr Zenaiev <oleksandr.zenaiev@gmail.com>
2026-06-22 19:14:19 +0200

    removed obs

--- reactions/BaseFFABM/ReactionBaseFFABM.cc
+++
@@ -1,7 +0,0 @@
-
-//#include "ReactionBaseFFABM.h"
-//#include "ReactionBaseDISNC.h"
-//#include "ReactionBaseDISCC.h"
-
-//template class ReactionBaseFFABM<ReactionBaseDISNC>;
-//template class ReactionBaseFFABM<ReactionBaseDISCC>;
