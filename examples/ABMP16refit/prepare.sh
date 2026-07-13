09e61a568800bfa1fde7aa0280942c4c7f7f0ff7
Oleksandr Zenaiev <oleksandr.zenaiev@gmail.com>
2026-07-13 19:01:34 +0200

    upd installation scrpt and test

--- examples/ABMP16refit/prepare.sh
+++ examples/ABMP16refit/prepare.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+
+if [ ! -d datafiles/fixedTarget/nomad ]; then
+rm datafiles
+wget "https://cernbox.cern.ch/remote.php/dav/public-files/grDRAZ7VnuwIjYw/abmp16-newdata.tar.gz"
+tar xvzpf abmp16-newdata.tar.gz
+rm abmp16-newdata.tar.gz
+fi
+
+if [ ! -d ../../fewzgrids ]; then
 wget "https://cernbox.cern.ch/remote.php/dav/public-files/CnmQytk8SYDCMJi/fewzgrids.tar.gz"
-tar xvzpf fewzgrids.tar.gz
+tar xvzpf fewzgrids.tar.gz -C ../..
+rm fewzgrids.tar.gz
+ln -s `pwd`/../../fewzgrids .
+else
+ln -s ../../fewzgrids
+fi
